81b8765f0172bee8137363ee268cdf1f7e02958f
Hannah Ahn <ahnlook90@gmail.com>
2023-03-06 15:56:21 +0900

    set eslint and pretier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.js', '.jsx'] },
+    ],
+  },
+}
