017422fff76be282b65b4168713e6863ac7e7422
realsnoopso <real@snoop.so>
2023-03-07 15:03:28 +0900

    Complete Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,44 @@ class Button extends HTMLElement {
   constructor() {
     super();
 
+    const text = this.innerText;
     const shadow = this.attachShadow({ mode: 'open' });
-    shadow.innerHTML = `
-      <button>test</button>
+
+    shadow.innerHTML = `<button>${text}</button>`;
+    if (type) {
+      this.shadowRoot.querySelector('button').classList.add(type);
+      this.shadowRoot.querySelector('button').classList.add('bold-sm');
+    }
+
+    this.shadowRoot.append(this.getStyle());
+  }
+
+  getStyle() {
+    const style = document.createElement('style');
+    const width = this.getAttribute('width');
+
+    style.textContent = `
+      button {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        padding: 0px 10px;
+        height: 32px;
+        width: ${width ? width + 'px' : 'auto'};
+        
+        background: linear-gradient(174.6deg, var(--white) 4.31%, var(--primary-yellow-100) 50.01%, var(--primary-yellow-200) 95.71%);
+
+        border: 1px solid var(--primary-yellow-300);
+        border-radius: 4px;
+
+        font-weight: var(--bold-sm-weight);
+        font-size: var(--bold-sm-size);
+        line-height: var(--bold-sm-height);
+        letter-spacing: var(--bold-sm-spacing);
+      }
     `;
+    return style;
   }
 }
 
