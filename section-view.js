13c697ca7e5b984cde0f5e90f65caceebf6152c8
sarang_daddy <sarang_daddy@naver.com>
2023-03-07 22:57:34 +0900

    접속 1초 후 로그인 레이아웃 출력
    - HTML : section__login__active -> section__login 으로 클래스명 변경
    - section__login에 hidden, visible 클래스 추가시 opacaity 0~1로 변경
    - section__login 1초 후 출력위해 js 페이지 생성
    - section-view.js에 setTimeout 이벤트로 1초 후 클래스명 변경 기능 추가

---
+++ section-view.js
@@ -0,0 +1,5 @@
+setTimeout(function () {
+  const loginSection = document.querySelector(".section__login");
+  loginSection.classList.add("visible");
+  loginSection.classList.remove("hidden");
+}, 1000);
