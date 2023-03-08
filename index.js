fbc07b3ff49df4478763ee1398cc6d8c2d1e5c64
zlx454545 <zlx454545@gmail.com>
2023-03-08 11:16:05 +0900

    Feat: "dimmed 처리 기능 구현"
    모달 혹은 레이어 팝업 시 dimmed 처리를 위한 레이어 생성.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const logInLink = document.querySelector(".header__link--login");
 const logInAnchor = document.querySelector(".header__anchor--login");
 const logInPopUp = document.querySelector(".pop-up--login");
 const seperatorLine = document.querySelector(".seperator-line");
 const list = document.querySelector(".list");
+const container = document.querySelector(".container");
+const dimmed = document.querySelector(".dimmed");
 
 function showPopUpWithDelay(delay) {
   setTimeout(() => {
@@ -14,10 +15,12 @@ function main() {
     seperatorLine.classList.remove("hidden");
     list.classList.remove("hidden");
     logInPopUp.classList.remove("hidden");
+    dimmed.classList.remove("hidden");
   });
 
-  logInLink.addEventListener("mouseleave", () => {
+  container.addEventListener("mouseleave", () => {
     logInPopUp.classList.add("hidden");
+    dimmed.classList.add("hidden");
   });
 
   showPopUpWithDelay(1000);
