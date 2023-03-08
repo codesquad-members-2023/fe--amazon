ee14f023cfd4631f1407d628e5f91180642f5e27
zlx454545 <zlx454545@gmail.com>
2023-03-08 10:56:18 +0900

    Feat: "로그인 레이어 팝업 기능 구현"
    헤더의 로그인 부분에 hover시 레이어 팝업 기능 구현."

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const logIn = document.querySelector(".header__anchor--login");
+const logInLink = document.querySelector(".header__link--login");
+const logInAnchor = document.querySelector(".header__anchor--login");
 const logInPopUp = document.querySelector(".pop-up--login");
+const seperatorLine = document.querySelector(".seperator-line");
+const list = document.querySelector(".list");
 
-function showPopUp() {
+function showPopUpWithDelay(delay) {
   setTimeout(() => {
     logInPopUp.classList.remove("hidden");
-  }, 1000);
+  }, delay);
 }
 function main() {
-  logIn.addEventListener("mouseover", () => {
+  logInAnchor.addEventListener("mouseenter", () => {
+    seperatorLine.classList.remove("hidden");
+    list.classList.remove("hidden");
+    logInPopUp.classList.remove("hidden");
+  });
+
+  logInLink.addEventListener("mouseleave", () => {
     logInPopUp.classList.add("hidden");
   });
 
-  showPopUp();
+  showPopUpWithDelay(1000);
 }
 
 main();
