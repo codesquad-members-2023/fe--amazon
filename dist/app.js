65f3cfe4d6268cda24deed5e915044fa6aa26115
JaydenLee1116 <vv55adss@gmail.com>
2023-03-08 21:55:56 +0900

    feat: Add login mouse event -ing
    첫 PR merge 후 중간 커밋입니다. 마우스 이벤트 구현 중입니다.

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
-const test = 1;
+const $ = document.querySelector.bind(document);
+const showLoginAdditional = () => {
+    $('.account-list').addEventListener('mouseover', (e) => {
+        $('.login__additional--hover').style.display = 'block';
+        $('.login__content').style.height = '1000%';
+        $('.wrapper__empty-gray').style.display = 'block';
+    });
+    $('.account-list').addEventListener('mouseout', (e) => {
+        $('.login__additional--hover').style.display = 'none';
+        $('.login__content').style.height = '300%';
+    });
+};
+showLoginAdditional();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-const test: number = 1;
+const $: (selector: string) => HTMLElement | null =
+  document.querySelector.bind(document);
+
+const showLoginAdditional = () => {
+  $('.account-list')!.addEventListener('mouseover', (e) => {
+    $('.login__additional--hover')!.style.display = 'block';
+    $('.login__content')!.style.height = '1000%';
+    $('.wrapper__empty-gray')!.style.display = 'block';
+  });
+  $('.account-list')!.addEventListener('mouseout', (e) => {
+    $('.login__additional--hover')!.style.display = 'none';
+    $('.login__content')!.style.height = '300%';
+  });
+};
+
+showLoginAdditional();
