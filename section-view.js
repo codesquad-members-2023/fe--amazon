344cb4c758ef4deeb73f3b468e1defdc371d3a97
sarang_daddy <sarang_daddy@naver.com>
2023-03-08 15:48:08 +0900

    login__section2 CSS 호버 삭제 ->  JS로 등록
    - mouseover, mouseout 으로 호버기능 생성
    - header__login에서 벗어나면 login__section__Deatail이 사라짐을 방지하기 위해

--- section-view.js
+++ section-view.js
@@ -1,5 +1,6 @@
 const headerLogin = document.querySelector(".home__header__login");
 const loginSection = document.querySelector(".section__login");
+const loginSectionDetail = document.querySelector(".section__login2");
 
 setTimeout(function () {
   loginSection.classList.add("visible");
@@ -11,4 +12,17 @@ const removeLoginSection = () => {
   loginSection.classList.add("hidden");
 };
 
+const showLoginSectionDetail = () => {
+  loginSectionDetail.style.display = "flex";
+};
+
+const hiddenLoginSectionDetail = () => {
+  loginSectionDetail.style.display = "none";
+};
+
 headerLogin.addEventListener("mouseout", removeLoginSection);
+
+headerLogin.addEventListener("mouseover", showLoginSectionDetail);
+loginSectionDetail.addEventListener("mouseover", showLoginSectionDetail);
+loginSectionDetail.addEventListener("mouseout", hiddenLoginSectionDetail);
+headerLogin.addEventListener("mouseout", hiddenLoginSectionDetail);
