391dd8ee0fd4b467bb30de1c073240d190d356d4
JangHoonSeok <orirori512@naver.com>
2023-03-08 15:17:46 +0900

    ✨ feat: 배송처 모달 이벤트 초기 설정

---
+++ js/shippingRegionEvent.js
@@ -0,0 +1,12 @@
+const shippingRegion = document.querySelector('.menu1');
+// const selectRegionModal = document.querySelector('');
+
+shippingRegion.addEventListener('mouseover', () => {
+  //   selectRegionModal.style.display = 'flex';
+  body.style.background = 'rgba(16, 20, 26, 0.4)';
+});
+
+shippingRegion.addEventListener('mouseout', () => {
+  //   selectRegionModal.style.display = 'none';
+  body.style.background = '';
+});
