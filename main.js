06bfa20f27dc9374b7b8efd0dd847034d82497bf
Hannah Ahn <ahnlook90@gmail.com>
2023-03-08 17:15:07 +0900

    Add modal for log in

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const logInModal = document.querySelector('.log-in .modal')
+
+window.addEventListener('DOMContentLoaded', () => {
+  // TODO: 1초 뒤에 애니메이션 효과와 함께 block 되게
+  logInModal.style.display = 'block'
+})
 
 const dim = document.querySelector('.dim')
 const shippingAddress = document.querySelector('.shipping-address')
