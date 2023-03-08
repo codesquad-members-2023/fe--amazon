85c67f744a7ccc5c1f4cd3deb7be2a1e6ae9cfc2
JangHoonSeok <orirori512@naver.com>
2023-03-08 14:22:59 +0900

    ✨ feat: 로그인 모달창 display none으로 구현 완료
    opacity로 구현하면 남아있어서 겹침

--- js/loginEvent.js
+++ js/loginEvent.js
@@ -1,10 +1,21 @@
 const loginMenu = document.querySelector('#loginMenu');
 const loginModal = document.querySelector('.nav_login');
 const hoverLoginModal = document.querySelector('.hover_login_modal');
+const loginSection = document.querySelector('.login_section');
 const body = document.querySelector('body');
 
 loginMenu.addEventListener('mouseover', () => {
-  loginModal.style.opacity = '0';
-  hoverLoginModal.style.opacity = '1';
+  //   loginModal.style.opacity = '0';
+  loginModal.style.display = 'none';
+  //   hoverLoginModal.style.opacity = '1';
+  hoverLoginModal.style.display = 'flex';
   body.style.background = 'rgba(16, 20, 26, 0.4)';
+});
+
+loginSection.addEventListener('mouseout', () => {
+  //   hoverLoginModal.style.opacity = '0';
+  hoverLoginModal.style.display = 'none';
+  //   loginModal.style.opacity = '1';
+  loginModal.style.display = 'flex';
+  body.style.background = '';
 });
