610620477f0f56852df53ce3222d8c9cad5310e1
sarang_daddy <sarang_daddy@naver.com>
2023-03-08 17:14:20 +0900

    딤처리 기능 추가
    - dim 효과를 가진 div생성 (최초 hidden) 상태
    - 위 div에 딤 효과 CSS 부여
    - hover 이벤트에 딤처리 이벤트 추가

--- section-view.js
+++ section-view.js
@@ -1,6 +1,7 @@
 const headerLogin = document.querySelector(".home__header__login");
 const loginSection = document.querySelector(".section__login");
 const loginSectionDetail = document.querySelector(".section__login2");
+const dimmDiv = document.querySelector(".dimmed");
 
 setTimeout(function () {
   loginSection.classList.add("visible");
@@ -14,15 +15,17 @@ const removeLoginSection = () => {
 
 const showLoginSectionDetail = () => {
   loginSectionDetail.style.display = "flex";
+  dimmDiv.classList.remove("hidden");
 };
 
 const hiddenLoginSectionDetail = () => {
   loginSectionDetail.style.display = "none";
+  dimmDiv.classList.add("hidden");
 };
 
 headerLogin.addEventListener("mouseout", removeLoginSection);
 
 headerLogin.addEventListener("mouseover", showLoginSectionDetail);
+headerLogin.addEventListener("mouseout", hiddenLoginSectionDetail);
 loginSectionDetail.addEventListener("mouseover", showLoginSectionDetail);
 loginSectionDetail.addEventListener("mouseout", hiddenLoginSectionDetail);
-headerLogin.addEventListener("mouseout", hiddenLoginSectionDetail);
