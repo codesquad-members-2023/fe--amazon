af1f1bbe6165574b872635d9eb263d64222d89ed
JangHoonSeok <orirori512@naver.com>
2023-03-09 22:40:32 +0900

    ✨ feat: 모두보기 버튼 완료
    TODO: 간격은 조정해야함.

--- js/sidebarEvent.js
+++ js/sidebarEvent.js
@@ -7,11 +7,14 @@ const closeButton = document.querySelector('#closeButton');
 allLink.addEventListener('click', () => {
   sideBar.style.display = 'flex';
   sideBar.style['z-index'] = '101';
+  sideBar.style.animation = 'fadeInLeft 1s forwards';
   dimLayer.style.display = 'block';
   dimLayer.style.background = 'rgba(16, 20, 26, 0.4)';
 });
 
 closeButton.addEventListener('click', () => {
-  sideBar.style.display = 'none';
+  sideBar.style.animation = 'fadeInRight 1s forwards';
+  sideBar.style['z-index'] = '';
+  //   sideBar.style.display = 'none';
   dimLayer.style.display = 'none';
 });
