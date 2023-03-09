c7a4f889d7505f6b08413f98e9ddd45909dbf061
JangHoonSeok <orirori512@naver.com>
2023-03-09 17:31:49 +0900

    ✨ feat: 사이드바를 눌렀을 시 전체 배경 딤처리 완료

--- js/sidebarEvent.js
+++ js/sidebarEvent.js
@@ -1,14 +1,17 @@
 const allLink = document.querySelector('.all_link');
 const sideBar = document.querySelector('.sidebar');
-
+const dimLayer = document.querySelector('.dim_layer');
+const header = document.querySelector('#head_navigation_bar');
 const closeButton = document.querySelector('#closeButton');
 
 allLink.addEventListener('click', () => {
   sideBar.style.display = 'flex';
-  body.style.background = 'rgba(16, 20, 26, 0.4)';
+  sideBar.style['z-index'] = '101';
+  dimLayer.style.display = 'block';
+  dimLayer.style.background = 'rgba(16, 20, 26, 0.4)';
 });
 
 closeButton.addEventListener('click', () => {
   sideBar.style.display = 'none';
-  body.style.background = '';
+  dimLayer.style.display = 'none';
 });
