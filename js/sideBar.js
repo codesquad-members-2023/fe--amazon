272e5e5484fc2fea040aed78a5fd8468b2eff0ac
Hannah Ahn <ahnlook90@gmail.com>
2023-03-09 15:29:12 +0900

    Implement closing side menu when click to another side

--- js/sideBar.js
+++ js/sideBar.js
@@ -11,3 +11,10 @@ const closeButton = document.querySelector('.side-menu__close')
 closeButton.addEventListener('click', () => {
   sideMenu.style.display = 'none'
 })
+
+const body = document.querySelector('body')
+body.addEventListener('click', (e) => {
+  if (!sideMenu.contains(e.target) && !openButton.contains(e.target)) {
+    sideMenu.style.display = 'none'
+  }
+})
