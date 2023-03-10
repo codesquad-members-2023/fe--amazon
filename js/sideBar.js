a0ccb82fcefc1dd32d452f950608673186a5b541
Hannah Ahn <ahnlook90@gmail.com>
2023-03-10 16:45:28 +0900

    Implement move to main from detail page

--- js/sideBar.js
+++ js/sideBar.js
@@ -1,50 +1,87 @@
-const openButton = document.querySelector('.all-menu')
 const sideMenu = document.querySelector('.side-menu')
-
-
-openButton.addEventListener('click', () => {
-  sideMenu.style.display = 'flex'
-  sideMenu.style.animation = 'slide-appear .5s'
-  dim.style.display = 'block'
-  dim.style.top = '-85px'
-})
-
-const closeButton = document.querySelector('.side-menu__close')
-
-closeButton.addEventListener('click', () => {
-  sideMenu.style.animation = 'slide-hide .5s forwards'
-  dim.style.display = 'none'
-})
-
-const body = document.querySelector('body')
-body.addEventListener('click', (e) => {
-  if (!sideMenu.contains(e.target) && !openButton.contains(e.target)) {
-    sideMenu.style.animation = 'slide-hide .5s forwards'
-    dim.style.display = 'none'
-  }
-})
-
+const openButton = document.querySelector('.all-menu')
+const sideMain = document.querySelector('.side-menu__main')
+const carGoodsPage = document.querySelector('.side-menu__car-goods')
+const simpleItemsButton = document.querySelector('.side-menu__hide-button')
 const allItems = document.querySelector('.side-menu__all-items')
 const allItemsButton = document.querySelector('.side-menu__show-button')
 
-allItemsButton.addEventListener('click', () => {
-  allItems.style.display = 'block'
-  allItems.style.animation = 'slow-appear 1s forwards'
-})
 
-const simpleItemsButton = document.querySelector('.side-menu__hide-button')
-simpleItemsButton.addEventListener('click', () => {
-  allItems.style.display = 'none'
-  allItems.style.animation = 'slow-hide 1s'
-})
+const openSideBar = () => {
 
-const carGoodsButton = allItems.querySelector('.item__car-goods')
-const carGoodsPage = document.querySelector('.side-menu__car-goods')
-const sideMain = document.querySelector('.side-menu__main')
+  openButton.addEventListener('click', () => {
+    sideMenu.style.display = 'flex'
+    sideMenu.style.animation = 'slide-appear .5s'
+    dim.style.display = 'block'
+    dim.style.top = '-85px'
+  })
+}
 
-carGoodsButton.addEventListener('click', () => {
-  sideMain.style.display = 'none'
-  sideMain.style.display = 'slide-hide .5s'
-  carGoodsPage.style.display = 'block'
-  carGoodsPage.style.animation = 'slide-appear-menu .5s forwards'
-})
+const hideSideBar = () => {
+  const closeButton = document.querySelector('.side-menu__close')
+  const body = document.querySelector('body')
+
+  closeButton.addEventListener('click', () => {
+    sideMenu.style.animation = 'slide-hide .5s forwards'
+    dim.style.display = 'none'
+  })
+
+  body.addEventListener('click', (e) => {
+    if (!sideMenu.contains(e.target) && !openButton.contains(e.target)) {
+      sideMenu.style.animation = 'slide-hide .5s forwards'
+      dim.style.display = 'none'
+    }
+  }) 
+}
+
+const oepnAllItems = () => {
+
+
+  allItemsButton.addEventListener('click', () => {
+    allItems.style.display = 'block'
+    allItems.style.animation = 'slow-appear-menu 1s forwards'
+  })
+}
+
+const closeAllItems = () => {
+
+  simpleItemsButton.addEventListener('click', () => {
+    allItems.style.display = 'none'
+    allItems.style.animation = 'slow-hide 1s'
+  })
+}
+
+const openItemMenu = () => {
+  const carGoodsButton = document.querySelector('.item__car-goods')
+
+  carGoodsButton.addEventListener('click', () => {
+    sideMain.style.display = 'none'
+    carGoodsPage.style.display = 'block'
+    carGoodsPage.style.animation = 'slide-appear-menu .5s forwards'
+  })
+}
+
+const moveMain = () => {
+  const backMainButton = document.querySelector('.back__main')
+  
+  backMainButton.addEventListener('click', () => {
+    sideMain.style.display = 'block'
+    carGoodsPage.style.display = 'none'
+    sideMain.style.animation = 'slide-appear .5s forwards'
+  })
+
+}
+
+const actSideBar = () => {
+  openSideBar()
+  hideSideBar()
+  oepnAllItems()
+  closeAllItems()
+  openItemMenu()
+  moveMain()
+}
+
+actSideBar()
+
+
+
