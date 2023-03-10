b5b78735d69798f2225a45b718533abfeb23a993
NANII <breadnanii@gmail.com>
2023-03-10 14:48:37 +0900

    ⚡️ feat : side bar 열고 닫힘 애니메이션 적용 완료
    - 모두 click, 왼쪽에서 오른쪽으로 이동
    - X click, 오른쪽에서 왼쪽으로 이동
    - li tag mouseEnter, 배경 색상 적용 (svg : 적용x)

--- javascript/sideBar.js
+++ javascript/sideBar.js
@@ -1,9 +1,12 @@
 const $every = document.getElementById('every');
 const $close = document.getElementById('side_bar_close');
 const $side_bar = document.querySelector('aside');
+const $side_bar_menu = document.querySelectorAll('.side_bar_container li');
+
 
 $every.addEventListener('click', e => {
   $side_bar.classList.add('expand');
+  $side_bar.classList.remove('contract');
   $side_bar.style.display = 'flex';
 });
 
@@ -11,6 +14,19 @@ $close.addEventListener('click', e => {
   $side_bar.classList.add('contract');
 });
 
-$side_bar.addEventListener('animationend', (e) =>{
-  if(e.animationName === 'contract') $side_bar.style.display = 'none'; //닫힌다음 사라지게하기
+$side_bar.addEventListener('animationend', e => {
+  if(e.animationName === 'contract') {
+    $side_bar.style.display = 'none';
+    $side_bar.classList.remove('expand');
+  } //닫힌다음 사라지게하기
 });
+
+$side_bar_menu.forEach(element => {
+  element.addEventListener('mouseenter', e => {
+    e.target.style.backgroundColor = 'rgba(179, 179, 179, 0.58)';
+  });
+
+  element.addEventListener('mouseleave', e => {
+    e.target.style.backgroundColor = 'inherit';
+  });
+})
