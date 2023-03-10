9f8e516f6cb138bcd63f90f6442a4307a586cedb
poco <godiva140@gmail.com>
2023-03-10 14:56:29 +0900

    feat: 마우스 hover 이벤트 발생시 dimmed 처리 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
-const loginContainer = document.querySelector('.login_container');
 const loginSection = document.querySelector('.login_section');
+const loginContainer = document.querySelector('.login_container');
 const loginPopup = document.querySelector('.login_pop-up');
 const dividingLine = document.querySelector('.dividing_line');
 const listContainer = document.querySelector('.list_container');
+const shippingSection = document.querySelector('.shipping_section');
+const shippingContainer = document.querySelector('.shipping_container');
+const shippingPopup = document.querySelector('.shipping_pop-up');
+const dimmedSection = document.querySelector('.dimmed');
 
 const showLoginInitLayer = () => {
   loginPopup.classList.remove('hidden');
@@ -12,10 +16,21 @@ const showLoginExtendedLayer = () => {
   loginPopup.classList.remove('hidden');
   dividingLine.classList.remove('hidden');
   listContainer.classList.remove('hidden');
+  dimmedSection.classList.remove('hidden');
 };
 
 const hideLoginExtendedLayer = () => {
   loginPopup.classList.add('hidden');
+  dimmedSection.classList.add('hidden');
+};
+
+const showShippingLayer = () => {
+  shippingPopup.classList.remove('hidden');
+  dimmedSection.classList.remove('hidden');
+};
+const hideShippingLayer = () => {
+  shippingPopup.classList.add('hidden');
+  dimmedSection.classList.add('hidden');
 };
 
 const main = () => {
@@ -24,6 +39,8 @@ const main = () => {
   }, 1000);
   loginContainer.addEventListener('mouseenter', showLoginExtendedLayer);
   loginSection.addEventListener('mouseleave', hideLoginExtendedLayer);
+  shippingContainer.addEventListener('mouseenter', showShippingLayer);
+  shippingSection.addEventListener('mouseleave', hideShippingLayer);
 };
 
 main();
