73723fe01fc13d5485d6f1d6046c44e87748fd37
NANII <breadnanii@gmail.com>
2023-03-12 06:50:19 +0900

    ✨ feat : side bar tab 상세 페이지 이동 애니메이션 적용

--- javascript/sideBar.js
+++ javascript/sideBar.js
@@ -5,7 +5,10 @@ const $side_bar_menu = document.querySelectorAll('.side_bar_container li');
 const $view_all = document.getElementById('view_all');
 const $view_close = document.getElementById('view_close');
 const $unfold_content = document.querySelector('.unfold_content');
+const $view_detail = document.querySelector('.view_detail');
 const $back_btn = document.getElementById('back_btn');
+const $car_supply_tab = document.getElementById('car_supply_tab');
+const $car_supply = document.getElementById('car_supply');
 
 $every.addEventListener('click', e => {
   const $modal_bg = document.querySelector('.modal_background');
@@ -42,4 +45,21 @@ $view_all.addEventListener('click',e => {
 $view_close.addEventListener('click', e => {
   $unfold_content.style.maxHeight = null;
   $view_all.lastElementChild.outerHTML = "<img src=\"/asset/images/vectors/Bottom.svg\">"
+});
+
+$car_supply_tab.addEventListener('click', e => {
+  $view_detail.classList.remove('fadeInLeftOut');
+  $view_detail.classList.add('fadeInLeft');
+  $view_detail.classList.remove('hidden');
+});
+
+$back_btn.addEventListener('click', e => {
+  $view_detail.classList.remove('fadeInLeft');
+  $view_detail.classList.add('fadeInLeftOut');
+});
+
+$view_detail.addEventListener('animationend', e => {
+  if(e.animationName === 'fadeInLeftOut') {
+    $view_detail.classList.add('hidden');
+  }
 });
