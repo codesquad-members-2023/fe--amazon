7cd4f8a6b2dea3813341875626ba319029d2bf2e
NANII <breadnanii@gmail.com>
2023-03-12 19:34:14 +0900

    ♻️ refactor : 1주차 코드 수정
    - css 중복 최소화
    - 불필요한 css style 제거
    - 클래스 네이밍

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,19 +1,16 @@
 const $header = document.querySelector('header');
 const $fade_in_popup = document.querySelector('.fade_in');
-const $login_bar = document.querySelector('.login_bar');
+const $login = document.querySelector('.login');
 const $login_popup_detail = document.querySelector('.login_popup_detail');
-const $shipping_address = document.querySelector('.shipping_address');
-const $shipping_address_popup = document.querySelector('.shipping_address_popup');
+const $shipping_address = document.querySelector('.address');
+const $shipping_address_popup = document.querySelector('.address_popup');
 const $modal_bg = document.querySelector('.modal_background');
 
-const init = () => {
-  setTimeout(() => {
-    $header.addEventListener('mouseenter', e => {
-      $fade_in_popup.classList.add('hidden');
-  }, 1500)});
-}
+$header.addEventListener('mouseenter', e => {
+  $fade_in_popup.classList.add('hidden');
+});
 
-$login_bar.addEventListener('mouseenter', e => {
+$login.addEventListener('mouseenter', e => {
   $login_popup_detail.classList.remove('hidden');
   $modal_bg.classList.remove('hidden');
 });
@@ -32,5 +29,3 @@ $shipping_address_popup.addEventListener('mouseleave', e => {
   $shipping_address_popup.classList.add('hidden');
   $modal_bg.classList.add('hidden');
 });
-
-init();
--- javascript/sideBar.js
+++ javascript/sideBar.js
@@ -1,65 +1,64 @@
-const $every = document.getElementById('every');
+const $every = document.querySelector('.every');
 const $close = document.getElementById('side_bar_close');
-const $side_bar = document.querySelector('aside');
-const $side_bar_menu = document.querySelectorAll('.side_bar_container li');
+const $side_bar = document.querySelector('.side_bar');
 const $view_all = document.getElementById('view_all');
-const $view_close = document.getElementById('view_close');
-const $unfold_content = document.querySelector('.unfold_content');
+const $view_simple = document.getElementById('view_simple');
+const $fold_content = document.querySelector('.fold_content');
 const $view_detail = document.querySelector('.view_detail');
 const $back_btn = document.getElementById('back_btn');
 const $car_supply_tab = document.getElementById('car_supply_tab');
-const $car_supply = document.getElementById('car_supply');
+// const $car_supply = document.getElementById('car_supply');
 
 $every.addEventListener('click', e => {
   const $modal_bg = document.querySelector('.modal_background');
-  $side_bar.classList.add('expand');
-  $side_bar.classList.remove('contract');
+  $side_bar.classList.add('slideRight');
+  $side_bar.classList.remove('slideLeft');
   $side_bar.style.display = 'flex';
   $modal_bg.classList.remove('hidden');
   $modal_bg.style.zIndex = '1';
 });
 
 $close.addEventListener('click', e => {
-  $side_bar.classList.add('contract');
+  $side_bar.classList.add('slideLeft');
 });
 
 $side_bar.addEventListener('animationend', e => {
-  if(e.animationName === 'contract') {
+  if(e.animationName === 'slideLeft' && e.target.className.includes('side_bar')) {
     $side_bar.style.display = 'none';
-    $side_bar.classList.remove('expand');
+    $side_bar.classList.remove('slideRight');
     $modal_bg.classList.add('hidden');
     $modal_bg.style.zIndex = 'inherit';
   }
 });
 
 $view_all.addEventListener('click',e => {
-  if ($unfold_content.style.maxHeight){
-    $unfold_content.style.maxHeight = null;
+  if ($fold_content.style.maxHeight){
+    $fold_content.style.maxHeight = null;
     $view_all.lastElementChild.outerHTML = "<img src=\"/asset/images/vectors/Bottom.svg\">"
   } else {
     $view_all.lastElementChild.outerHTML = "<img src=\"/asset/images/vectors/Up.svg\">"
-    $unfold_content.style.maxHeight = $unfold_content.scrollHeight + "px";
+    $fold_content.style.maxHeight = $fold_content.scrollHeight + "px";
   }
 });
 
-$view_close.addEventListener('click', e => {
-  $unfold_content.style.maxHeight = null;
+$view_simple.addEventListener('click', e => {
+  $fold_content.style.maxHeight = null;
   $view_all.lastElementChild.outerHTML = "<img src=\"/asset/images/vectors/Bottom.svg\">"
 });
 
 $car_supply_tab.addEventListener('click', e => {
-  $view_detail.classList.remove('fadeInLeftOut');
-  $view_detail.classList.add('fadeInLeft');
+  $view_detail.classList.remove('slideLeft');
+  $view_detail.classList.add('slideRight');
   $view_detail.classList.remove('hidden');
 });
 
 $back_btn.addEventListener('click', e => {
-  $view_detail.classList.remove('fadeInLeft');
-  $view_detail.classList.add('fadeInLeftOut');
+  $view_detail.classList.remove('slideRight');
+  $view_detail.classList.add('slideLeft');
 });
 
 $view_detail.addEventListener('animationend', e => {
-  if(e.animationName === 'fadeInLeftOut') {
+  if(e.animationName === 'slideLeft' && e.target.className.includes('view_detail')) {
     $view_detail.classList.add('hidden');
   }
 });
