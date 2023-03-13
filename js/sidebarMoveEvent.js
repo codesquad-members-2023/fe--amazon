1f6424c4ce24968cadd9abfd16403c78936b9d79
JangHoonSeok <orirori512@naver.com>
2023-03-13 11:23:04 +0900

    ✨ feat: 사이드바 click 이벤트 완료

--- js/sidebarMoveEvent.js
+++ js/sidebarMoveEvent.js
@@ -1,19 +1,31 @@
 const mainMenuArr = document.querySelectorAll('.sidebar_main_section');
 const detailMenu = document.querySelector('.sidebar_detail_section');
+const backButton = document.querySelector('.sidebar_detail_back_button');
 
 [...mainMenuArr].forEach((mainMenu) => {
   mainMenu.addEventListener('click', (e) => {
-    console.log(e);
+    // console.log(e);
     if (
       !e.target.innerText.includes('모두 보기') &&
       e.target.innerText !== '간단히 보기'
     ) {
-      console.log('이지은바보');
       [...mainMenuArr].map((menu) => {
         menu.style.display = 'none';
       });
-      // detailMenu.style.animation = 'fadeInDetail 1s forwards';
+      detailMenu.style.animation = 'appearDetail 0.5s forwards';
       detailMenu.style.display = 'block';
     }
   });
 });
+
+backButton.addEventListener('click', () => {
+  detailMenu.style.display = 'none';
+  [...mainMenuArr].forEach((mainMenu) => {
+    mainMenu.style.display = 'block';
+    mainMenu.style.animation = 'hideDetail 0.5s forwards';
+  });
+});
+
+// detailMenu.addEventListener('animationend', (e) => {
+//   if (e.animationName === 'hideDetail') detailMenu.style.display = 'none';
+// });
