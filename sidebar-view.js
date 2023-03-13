546335278f67615d10e4ed2e9a5e7149fb8fa8cd
sarang_daddy <sarang_daddy@naver.com>
2023-03-13 13:23:56 +0900

    자바스크립트 전역변수 제거
    - init함수 생성하여 init에 포함시켜주었다.
    - 불필요한 변수 제거

--- sidebar-view.js
+++ sidebar-view.js
@@ -1,48 +1,52 @@
-const homeSide = document.querySelector(".homeSidebar");
-const homeSideOpen = document.querySelector(".homeMenu__all");
-const homeSideClose = document.querySelector(".homeSidebar__closeBtn");
-const dimmDivIndex5 = document.querySelector(".dimmed--zIndex5");
+const sidebarViewInit = () => {
+  const homeSide = document.querySelector(".homeSidebar");
+  const homeSideOpen = document.querySelector(".homeMenu__all");
+  const homeSideClose = document.querySelector(".homeSidebar__closeBtn");
+  const dimmDivIndex5 = document.querySelector(".dimmed--zIndex5");
 
-const openSide = () => {
-  homeSide.style.display = "flex";
-  homeSide.classList.remove("hideAnimation");
-  homeSide.classList.add("showAnimation");
-  dimmDivIndex5.classList.remove("hidden");
+  const openSide = () => {
+    homeSide.style.display = "flex";
+    homeSide.classList.remove("hideAnimation");
+    homeSide.classList.add("showAnimation");
+    dimmDivIndex5.classList.remove("hidden");
+  };
+
+  const hiddenSide = () => {
+    homeSide.classList.remove("showAnimation");
+    homeSide.classList.add("hideAnimation");
+    dimmDivIndex5.classList.add("hidden");
+  };
+
+  const toDisplayNone = (e) => {
+    if (e.animationName === "hideSidebarSoft") {
+      homeSide.style.display = "none";
+    }
+  };
+
+  homeSideOpen.addEventListener("click", openSide);
+  homeSideClose.addEventListener("click", hiddenSide);
+  homeSide.addEventListener("animationend", toDisplayNone);
+
+  const homeSidePlus = document.querySelector(".hideLists");
+  const homeSideSeeAll = document.querySelector(".homeSidebar__mode--open");
+  const homeSideCloseAll = document.querySelector(".homeSidebar__mode--close");
+
+  const openSideMore = () => {
+    homeSidePlus.style.display = "block";
+    homeSidePlus.classList.remove("closeAnimation");
+    homeSidePlus.classList.add("openAnimation");
+  };
+
+  const closeSideMore = () => {
+    homeSidePlus.classList.remove("openAnimation");
+    homeSidePlus.classList.add("closeAnimation");
+    setTimeout(() => {
+      homeSidePlus.style.display = "none";
+    }, 500);
+  };
+
+  homeSideSeeAll.addEventListener("click", openSideMore);
+  homeSideCloseAll.addEventListener("click", closeSideMore);
 };
 
-const hiddenSide = () => {
-  homeSide.classList.remove("showAnimation");
-  homeSide.classList.add("hideAnimation");
-  dimmDivIndex5.classList.add("hidden");
-};
-
-const toDisplayNone = (e) => {
-  if (e.animationName === "hideSidebarSoft") {
-    homeSide.style.display = "none";
-  }
-};
-
-homeSideOpen.addEventListener("click", openSide);
-homeSideClose.addEventListener("click", hiddenSide);
-homeSide.addEventListener("animationend", toDisplayNone);
-
-const homeSidePlus = document.querySelector(".hideLists");
-const homeSideSeeAll = document.querySelector(".homeSidebar__mode--open");
-const homeSideCloseAll = document.querySelector(".homeSidebar__mode--close");
-
-const openSideMore = () => {
-  homeSidePlus.style.display = "block";
-  homeSidePlus.classList.remove("closeAnimation");
-  homeSidePlus.classList.add("openAnimation");
-};
-
-const closeSideMore = () => {
-  homeSidePlus.classList.remove("openAnimation");
-  homeSidePlus.classList.add("closeAnimation");
-  setTimeout(() => {
-    homeSidePlus.style.display = "none";
-  }, 500);
-};
-
-homeSideSeeAll.addEventListener("click", openSideMore);
-homeSideCloseAll.addEventListener("click", closeSideMore);
+sidebarViewInit();
