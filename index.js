0808c6ea271f5e514972519370eb04f411e53f93
sjuhan123 <sjuhan123@gmail.com>
2023-03-13 11:27:37 +0900

    feat. 사이드바 레이아웃 및 애니메이션 구현
    extra menu 레이아웃 및 애니메이션 구현
    right menu 레이아웃 및 애니메이션 구현

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
-const loginPointer = document.querySelector(".modal__pointer1");
-const loginModalLoad = document.querySelector(".modal__login1");
+const loginPointer1 = document.querySelector(".modal__pointer1");
+const loginModal1 = document.querySelector(".modal__login1");
+
+// login 버튼 영역 변수화
+const loginPath2 = document.querySelector(".modal__path2");
+const loginPointer2 = document.querySelector(".modal__pointer2");
+const loginModal2 = document.querySelector(".modal__login2");
+
 
 (function (delay) {
   setTimeout(function() {
-    loginPointer.style.display = 'block';
-    loginModalLoad.style.display = 'block';
+    loginPointer1.style.display = 'block';
+    loginModal1.style.display = 'block';
   }, delay)
 })(1000);
 
 const loginButton = document.querySelector(".nav-top__account-link");
-const loginModal1 = document.querySelector(".modal__login1");
 const dimLayer = document.querySelector(".dim__layer");
 
 loginButton.addEventListener("mouseover", () => {
   loginModal1.style.display = 'none';
+  loginModal2.style.display = 'block';
+  loginPointer2.style.display = 'block';
+  loginPath2.style.display = 'block';
   dimLayer.style.display = 'block';
+})
+
+const loginModal2Layout = [loginPath2, loginModal2];
+
+loginButton.addEventListener("mouseout", () => {
+  loginModal2.style.display = 'none';
+  loginPointer2.style.display = 'none';
+})
+
+loginPath2.addEventListener("mouseover", () => {
+  loginModal2.style.display = 'block';
+  loginPointer2.style.display = 'block';
+})
+
+loginModal2.addEventListener("mouseover", () => {
+  loginModal2.style.display = 'block';
+  loginPointer2.style.display = 'block';
+})
+
+loginPath2.addEventListener("mouseout", () => {
+  loginModal2.style.display = 'none';
+  loginPointer2.style.display = 'none';
+})
+
+loginModal2.addEventListener("mouseout", () => {
+  loginModal2.style.display = 'none';
+  loginPointer2.style.display = 'none';
 })
 
 /*     딤처리 부분      */
@@ -22,11 +57,7 @@ loginButton.addEventListener("mouseover", () => {
 const locationButton = document.querySelector(".nav-top__link");
 const locationModal = document.querySelector(".modal__location");
 
-// login 버튼 영역 변수화
-const loginModal2 = document.querySelector(".modal__login2");
-const loginPath = document.querySelector(".modal__path2");
-
-const modalLayouts = [locationButton, locationModal, loginButton, loginModal2, loginPath];
+const modalLayouts = [locationButton, locationModal, loginButton, loginModal2, loginPath2];
 
 modalLayouts.forEach((node) => {
   node.addEventListener("mouseover", () => {
@@ -36,22 +67,62 @@ modalLayouts.forEach((node) => {
 
 modalLayouts.forEach((node) => {
   node.addEventListener("mouseout", () => {
-    if(sideBar.style.display != 'block') dimLayer.style.display = 'none';
+    if(sideBarLayer.style['animation-name'] != 'slideRightWard') dimLayer.style.display = 'none';
   })
 })
 
-/* sidebar 버튼 및 레이아웃 영역 */
+/**
+ * sidebar 버튼 및 레이아웃 영역  
+ */
 
 const sideBarOpenBtn = document.querySelector(".nav-bottom__selectAll");
-const sideBar = document.querySelector(".sidebar-elements");
-const sideBarCloseBtn = document.querySelector(".sidebar__closeBtn");
+const sideBarLayer = document.querySelector(".sidebar__layer");
+const sideBarCloseBtn = document.querySelector(".sidebar__closeBtn--container");
+const sideBarCloseBtnLayer = document.querySelector(".sidebar__closeBtn")
 
 sideBarOpenBtn.addEventListener('click', () => {
-  sideBar.style.display = 'block';
+  sideBarLayer.style['animation-name'] = 'slideRightWard';
   dimLayer.style.display = 'block';
 });
 
 sideBarCloseBtn.addEventListener('click', () => {
-  sideBar.style.display = 'none';
+  sideBarLayer.style['animation-name'] = 'slideLeftWard';
+
   dimLayer.style.display = 'none';
+})
+
+// slidebar shopping extra menu 영역
+
+const sideBarShowAllBtn = document.querySelector('.sidebar__contents.btn.showall');
+const sideBarCloseAllBtn = document.querySelector('.sidebar__contents.btn.closeall');
+const sideBarExtraShoppingList = document.querySelector('.sidebar__shopping.extra');
+
+sideBarShowAllBtn.addEventListener('click', () => {
+  // sideBarExtraShoppingList.style.height = '100%';
+  sideBarExtraShoppingList.classList.add('expended');
+})
+
+sideBarCloseAllBtn.addEventListener('click', () => {
+  // sideBarExtraShoppingList.style.height = '0';
+  sideBarExtraShoppingList.classList.remove('expended');
+})
+
+// sidebar menu right 영역
+
+//  열기
+const sideBarMenuMain = document.querySelector('.sidebar__menu.main');
+const sideBarMenuRight = document.querySelector('.sidebar__menu.right');
+const sideBarShoppingElectronBtn = document.querySelector('.sidebar__contents-shopping__electron');
+
+sideBarShoppingElectronBtn.addEventListener('click', () => {
+  sideBarMenuMain.style['animation-name'] = 'slideMenuLeftWard';
+  sideBarMenuRight.style['animation-name'] = 'slideMenuLeftWard';
+})
+
+//닫기
+const sideBarMenuRightCloseBtn = document.querySelector('.sidebar__contents.btn.close-right-menu');
+
+sideBarMenuRightCloseBtn.addEventListener('click', () => {
+  sideBarMenuMain.style['animation-name'] = 'slideMenuRightWard';
+  sideBarMenuRight.style['animation-name'] = 'slideMenuRightWard';
 })
