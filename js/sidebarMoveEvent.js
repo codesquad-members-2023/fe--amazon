cbd62babe4ec2c6772bc86d5be02ec362d55d171
JangHoonSeok <orirori512@naver.com>
2023-03-13 17:14:24 +0900

    🐛 bug : 클릭 이벤트가 단 한번밖에 발생하지 않음

--- js/sidebarMoveEvent.js
+++ js/sidebarMoveEvent.js
@@ -1,32 +1,69 @@
 import SIDEBAR_DETAIL from '../data/sidebarData.js';
 
-console.log(SIDEBAR_DETAIL);
-const mainMenuArr = document.querySelectorAll('.sidebar_main_section');
-const detailMenu = document.querySelector('.sidebar_detail_section');
-const backButton = document.querySelector('.sidebar_detail_back_button');
+let mainMenuArr = document.querySelectorAll('.sidebar_main_section');
+const sidebar = document.querySelector('.sidebar');
+const sidebarData = SIDEBAR_DETAIL.SIDEBAR_DETAIL;
 
-[...mainMenuArr].forEach((mainMenu) => {
+function createHTML(title, data) {
+  let htmlForm = `
+  <section class="sidebar_section sidebar_detail_section">
+    <a href="#">
+      <div class="sidebar_detail_back_button">
+        <img src="asset/left_arrow_icon.svg" />
+        <span>주메뉴</span>
+      </div>
+    </a>
+
+    <header class="sidebar_section_header">${title}</header>
+    <ul class="sidebar_section_list">`;
+
+  data.forEach((menuName) => {
+    htmlForm += `<a href="#">
+    <li>
+    <div>${menuName}</div>
+    <img src="asset/right_arrow_icon.svg" />
+    </li>
+    </a>`;
+  });
+
+  htmlForm += `</ul></section>`;
+  return htmlForm;
+}
+
+mainMenuArr.forEach((mainMenu) => {
   mainMenu.addEventListener('click', (e) => {
+    console.log(e.target, e.currentTarget);
     if (
       !e.target.innerText.includes('모두 보기') &&
       e.target.innerText !== '간단히 보기'
     ) {
       const title = e.target.innerText;
-      console.log(SIDEBAR_DETAIL.SIDEBAR_DETAIL[title]);
-      [...mainMenuArr].forEach((menu) => {
+      const data = sidebarData[e.target.innerText];
+
+      mainMenuArr.forEach((menu) => {
         menu.style.display = 'none';
+        // menu.style.opacity = 0;
+        // menu.style['z-index'] = '-2';
       });
+
+      sidebar.innerHTML += createHTML(title, data);
+      const detailMenu = document.querySelector('.sidebar_detail_section');
       detailMenu.style.animation = 'appearDetail 0.5s forwards';
       detailMenu.style.display = 'block';
-    }
-  });
-});
 
-backButton.addEventListener('click', () => {
-  detailMenu.style.display = 'none';
-  [...mainMenuArr].forEach((mainMenu) => {
-    mainMenu.style.display = 'block';
-    mainMenu.style.animation = 'hideDetail 0.5s forwards';
+      const backButton = document.querySelector('.sidebar_detail_back_button');
+      backButton.addEventListener('click', () => {
+        detailMenu.style.display = 'none';
+        mainMenuArr = document.querySelectorAll('.sidebar_main_section');
+        mainMenuArr.forEach((mainMenu) => {
+          mainMenu.style.display = 'block';
+          // console.log(mainMenu);
+          // mainMenu.style.opacity = 1;
+          // mainMenu.style['z-index'] = '';
+          mainMenu.style.animation = 'hideDetail 0.5s forwards';
+        });
+      });
+    }
   });
 });
 
