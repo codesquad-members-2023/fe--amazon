537ea011e8f02d8aef6bbadf0fd86ae18fb2212a
yesolyo <ahnyelim77@gmail.com>
2023-03-13 17:23:49 +0900

    fix: login 팝업창과 배송처 팝업 창 hover 시 header 제외 dimmed 처리
    기존에 header도 포함하여 dimmed처리가 되었으나 header를 제외하여
    dimmed 처리로 변경하였습니다.(로그인 팝업창과 배송처 팝업창 hover시)

--- js/dimmed.js
+++ js/dimmed.js
@@ -1,4 +1,5 @@
 const loginMenu = document.querySelector(".nav__login_popup-box");
+const shippingMenu = document.querySelector(".nav__shipping");
 const dimmed = document.querySelector(".dim-layer");
 
 loginMenu.addEventListener("mouseover", () => {
@@ -6,5 +7,13 @@ loginMenu.addEventListener("mouseover", () => {
 });
 
 loginMenu.addEventListener("mouseout", () => {
+  dimmed.style.display = "none";
+});
+
+shippingMenu.addEventListener("mouseover", () => {
+  dimmed.style.display = "block";
+});
+
+shippingMenu.addEventListener("mouseout", () => {
   dimmed.style.display = "none";
 });
