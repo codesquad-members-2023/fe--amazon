1fd2d082cc324c178f10a2f153bd0db0b4ff5935
saseung <seung0122@naver.com>
2023-03-14 22:25:16 +0900

    ✨ feat: 사이드바 메뉴영역 클릭 이벤트 구현
    사이드바 '모두보기' 클릭시 하위 영역 펼쳐지는 기능 구현

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ setTimeout(() => {
   const el = document.querySelector(".login-modal");
   el.classList.add("visible");
 }, 1000);
+
+const sidebarView = document.querySelector(".view-all-contents");
+const sidebarOtherContents = document.querySelector(".sidebar-other-contents");
+
+sidebarView.addEventListener("click", (e) => {
+  sidebarOtherContents.style.height = "100%";
+});
+
+sidebarView.addEventListener("dblclick", (e) => {
+  sidebarOtherContents.style.height = "0%";
+});
