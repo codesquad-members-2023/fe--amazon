45ed3a44f29eb5c9b2b7d2d0d970c6666ba328e0
sarang_daddy <sarang_daddy@naver.com>
2023-03-14 16:42:24 +0900

    사이드바 디테일 페이지 레이아웃 생성
    - 새로운 <aside>를 생성하여 기존 레이아웃 스타일 그대로 가져옴
    - "주 메뉴" 돌아가기 버튼 클릭시 사이드바로 돌아감
    - 버튼 이벤트는 상위 ul에 위임하여 구현함

--- js/sideBarDetail.js
+++ js/sideBarDetail.js
@@ -1,1 +1,11 @@
-console.log(SIDEBAR_DETAIL);
+const detailSideBar = document.querySelector(".homeSidebar__detail");
+const backButton = document.querySelector(".homeSidebar__mode--back");
+
+const selecteList = (event) => {
+  console.log(event.target);
+  if (event.target === backButton) {
+    detailSideBar.classList.remove("hideDetail");
+  }
+};
+
+detailSideBar.addEventListener("click", selecteList);
