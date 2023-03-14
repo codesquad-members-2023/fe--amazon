8b35b7332d1b1c6105a603555cbfb01e55a1036e
yousooa <kinsk2839@naver.com>
2023-03-14 14:27:59 +0900

    feat: 사이드바 열었을 때 딤처리
    - 사이드바와 딤화면 태그에 z-index 처리
    - close 버튼 색깔 검은색에서 흰색으로 변경

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import { $, $$ } from './util/dom.js';
 
 const main = () => {
+  const $dimCover = $('.dim-cover');
   const $login = $('.login');
   const $loginNav = $('.login_section', $login);
   const $loginModal = $('.login_modal', $login);
@@ -20,10 +21,12 @@ const main = () => {
   $sideBarBtn.addEventListener('click', () => {
     if ($sideBar.classList.contains('close_side_bar')) $sideBar.classList.remove('close_side_bar');
     $sideBar.classList.add('open_side_bar');
+    $dimCover.classList.remove('hidden');
   });
   $closeSideBarBtn.addEventListener('click', () => {
     if ($sideBar.classList.contains('open_side_bar')) $sideBar.classList.remove('open_side_bar');
     $sideBar.classList.add('close_side_bar');
+    $dimCover.classList.add('hidden');
   });
 
   const $expandedShoppingCategoryBtn = $('.expanded_shopping_category_btn', $sideBar);
