947940db2f19d313a12860db625edce4636358a0
sarang_daddy <sarang_daddy@naver.com>
2023-03-15 10:21:20 +0900

    사이드바 세부페이지 레이아웃 생성
    - 사이드바 리스트 클릭시 세부페이지로 이동
    - 세부페이지 flex <-> hidden 클래스 추가 삭제로 호출

--- js/sideBarDetail.js
+++ js/sideBarDetail.js
@@ -1,11 +1,30 @@
 const detailSideBar = document.querySelector(".homeSidebar__detail");
 const backButton = document.querySelector(".homeSidebar__mode--back");
 
-const selecteList = (event) => {
+const selectListInDetail = (event) => {
   console.log(event.target);
-  if (event.target === backButton) {
+  if (backButton.contains(event.target)) {
+    detailSideBar.classList.add("hideDetail");
+  }
+};
+
+detailSideBar.addEventListener("click", selectListInDetail);
+
+const sideBar = document.querySelector(".homeSidebar");
+const sideBarList = document.querySelector(".homeSidebar__subTitle");
+
+const aaa = sideBarList.innerText;
+
+// const sideBarListArray = [...sideBarList];
+
+// console.log(sideBarListArray);
+
+const selectListInSidebar = (event) => {
+  const title = event.target.innerText;
+  console.log(title);
+  if (SIDEBAR_DETAIL[title]) {
     detailSideBar.classList.remove("hideDetail");
   }
 };
 
-detailSideBar.addEventListener("click", selecteList);
+sideBar.addEventListener("click", selectListInSidebar);
