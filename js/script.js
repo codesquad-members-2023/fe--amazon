1a9183a9b109df6bb29a6644ba048857b898ecfa
saseung <seung0122@naver.com>
2023-03-15 21:27:06 +0900

    ✨ feat: 사이드바, 배송처 hover시 딤처리 적용
    JavaScript를 사용하여 요소의 클래스를 변경해 딤처리 효과를 적용하도록 수정

--- js/script.js
+++ js/script.js
@@ -5,10 +5,14 @@ setTimeout(() => {
 
 const sidebarButton = document.querySelector(".nav-sidebar-button");
 const sidebar = document.querySelector(".sidebar");
+const dimmed = document.querySelector(".dimmed");
+const loginModal = document.querySelector(".login-modal");
 
 sidebarButton.addEventListener("click", () => {
   sidebar.style.left = "0";
   closeButton.style.opacity = "100%";
+  dimmed.style.display = "block";
+  loginModal.style.display = "none";
 });
 
 const sidebarView = document.querySelector(".view-all-contents");
@@ -29,13 +33,13 @@ const sidebarCloseButton = document.querySelector(".sidebar-close-button");
 sidebarCloseButton.addEventListener("click", () => {
   sidebar.style.left = "-400px";
   closeButton.style.opacity = "0%";
+  dimmed.style.display = "none";
 });
 
 // 로그인 호버, 딤처리
-const dimmed = document.querySelector(".dimmed");
+
 const loginMenu = document.querySelector(".login-menu");
 const loginHover = document.querySelector(".login-hover");
-const loginModal = document.querySelector(".login-modal");
 
 loginMenu.addEventListener("mouseover", () => {
   loginHover.style.display = "block";
@@ -45,5 +49,19 @@ loginMenu.addEventListener("mouseover", () => {
 loginMenu.addEventListener("mouseout", () => {
   loginHover.style.display = "none";
   loginModal.style.display = "none";
+  dimmed.style.display = "none";
+});
+
+const shippingHover = document.querySelector(".shipping-hover");
+const shippingCountry = document.querySelector(".shipping-country");
+
+shippingCountry.addEventListener("mouseover", () => {
+  shippingHover.style.display = "block";
+  dimmed.style.display = "block";
+  loginModal.style.display = "none";
+});
+
+shippingCountry.addEventListener("mouseout", () => {
+  shippingHover.style.display = "none";
   dimmed.style.display = "none";
 });
