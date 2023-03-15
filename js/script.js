53aaa1f5bdd085ec16493b6769468dcf4c24a2ff
saseung <seung0122@naver.com>
2023-03-15 21:05:39 +0900

    ✨ feat: 로그인 메뉴바 hover시 딤처리 적용
    로그인 모달 창에서 메뉴바로 호버 기능 변경, JavaScript를 사용하여 요소의 클래스를 변경해 딤처리 효과를 적용하도록 수정

--- js/script.js
+++ js/script.js
@@ -8,11 +8,13 @@ const sidebar = document.querySelector(".sidebar");
 
 sidebarButton.addEventListener("click", () => {
   sidebar.style.left = "0";
+  closeButton.style.opacity = "100%";
 });
 
 const sidebarView = document.querySelector(".view-all-contents");
 const sidebarOtherContents = document.querySelector(".sidebar-other-contents");
 const sidebarQuickView = document.querySelector(".quick-view");
+const closeButton = document.querySelector(".sidebar-close-button");
 
 sidebarView.addEventListener("click", () => {
   sidebarOtherContents.style.height = "100%";
@@ -26,4 +28,22 @@ const sidebarCloseButton = document.querySelector(".sidebar-close-button");
 
 sidebarCloseButton.addEventListener("click", () => {
   sidebar.style.left = "-400px";
+  closeButton.style.opacity = "0%";
+});
+
+// 로그인 호버, 딤처리
+const dimmed = document.querySelector(".dimmed");
+const loginMenu = document.querySelector(".login-menu");
+const loginHover = document.querySelector(".login-hover");
+const loginModal = document.querySelector(".login-modal");
+
+loginMenu.addEventListener("mouseover", () => {
+  loginHover.style.display = "block";
+  dimmed.style.display = "block";
+});
+
+loginMenu.addEventListener("mouseout", () => {
+  loginHover.style.display = "none";
+  loginModal.style.display = "none";
+  dimmed.style.display = "none";
 });
