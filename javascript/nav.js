8e195106a5d1d819a7cd5234b92931e79c337f99
NANII <breadnanii@gmail.com>
2023-03-16 23:53:21 +0900

    ♻️ feat : nav.js 전역변수 -> 함수안으로 옮김
    - 매개변수, 구조분해할당해야됨
    - 중복 최소화해야됨

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,31 +1,48 @@
-const $header = document.querySelector('header');
-const $fade_in_popup = document.querySelector('.fade_in');
-const $login = document.querySelector('.login');
-const $login_popup_detail = document.querySelector('.login_popup_detail');
-const $shipping_address = document.querySelector('.address');
-const $shipping_address_popup = document.querySelector('.address_popup');
-const $modal_bg = document.querySelector('.modal_background');
+const navBarEventHandler = () => {
+  const $header = document.querySelector('header');
+  const $login = document.querySelector('.login');
+  const $shipping_address = document.querySelector('.address');
+  const $shipping_address_popup = document.querySelector('.address_popup');
+  const $login_popup_detail = document.querySelector('.login_popup_detail');
 
-$header.addEventListener('mouseenter', e => {
+  $header.addEventListener('mouseenter', hidePopup);
+  $login.addEventListener('mouseenter', PopupLoginModal);
+  $shipping_address.addEventListener('mouseenter', popUpShippingAddressModal);
+  $shipping_address_popup.addEventListener('mouseleave', hideShippingAddress);
+  $login_popup_detail.addEventListener('mouseleave', hideDetailLoginModal);
+}
+
+const hidePopup = e => {
+  const $fade_in_popup = document.querySelector('.fade_in');
   $fade_in_popup.classList.add('hidden');
-});
+}
 
-$login.addEventListener('mouseenter', e => {
+const PopupLoginModal = e => {
+  const $login_popup_detail = document.querySelector('.login_popup_detail');
+  const $modal_bg = document.querySelector('.modal_background');
   $login_popup_detail.classList.remove('hidden');
   $modal_bg.classList.remove('hidden');
-});
+}
 
-$shipping_address.addEventListener('mouseenter', e => {
+const popUpShippingAddressModal = e => {
+  const $shipping_address_popup = document.querySelector('.address_popup');
+  const $modal_bg = document.querySelector('.modal_background');
   $shipping_address_popup.classList.remove('hidden');
   $modal_bg.classList.remove('hidden');
-});
+}
 
-$login_popup_detail.addEventListener('mouseleave', e => {
+const hideDetailLoginModal = e => {
+  const $login_popup_detail = document.querySelector('.login_popup_detail');
+  const $modal_bg = document.querySelector('.modal_background');
   $login_popup_detail.classList.add('hidden');
   $modal_bg.classList.add('hidden');
-});
+}
 
-$shipping_address_popup.addEventListener('mouseleave', e => {
+const hideShippingAddress = e => {
+  const $shipping_address_popup = document.querySelector('.address_popup');
+  const $modal_bg = document.querySelector('.modal_background');
   $shipping_address_popup.classList.add('hidden');
   $modal_bg.classList.add('hidden');
-});
+}
+
+navBarEventHandler();
