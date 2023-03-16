4cf09ffaacf6fbb4db73a7b2d18abdb8529ee260
yousooa <kinsk2839@naver.com>
2023-03-16 18:37:00 +0900

    feat: header의 로그인 영역 딤처리 기능 구현
    - z-index 적용
    - 사이드바 dim 화면과 header dim 화면 각각 태그 생성

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,39 @@
 import { $, $$ } from './util/dom.js';
 
 const main = () => {
-  const $dimCover = $('.dim-cover');
   const $login = $('.login');
-  const $loginNav = $('.login_section', $login);
   const $loginModal = $('.login_modal', $login);
-  const $loginModalMore = $('.login_modal_more', $loginModal);
+  const $headerDimCover = $('.dim_cover_in_header');
 
-  $loginNav.addEventListener('mouseenter', () => {
-    $loginModalMore.classList.remove('hidden');
+  $login.addEventListener('mouseenter', () => {
+    $loginModal.classList.remove('hidden');
+    $headerDimCover.classList.remove('hidden');
   });
-  $loginNav.addEventListener('mouseleave', () => {
-    $loginModalMore.classList.add('hidden');
+  $login.addEventListener('mouseleave', () => {
+    $loginModal.classList.add('hidden');
+    $headerDimCover.classList.add('hidden');
   });
 
   const $sideBarBtn = $('.side_bar_btn');
   const $sideBar = $('.side_bar');
   const $closeSideBarBtn = $('.close_side_bar_btn', $sideBar);
+  const $sideBarDimCover = $('.dim_cover_in_sideBar');
 
   $sideBarBtn.addEventListener('click', () => {
     if ($sideBar.classList.contains('close_side_bar')) $sideBar.classList.remove('close_side_bar');
     $sideBar.classList.add('open_side_bar');
-    $dimCover.classList.remove('hidden');
+    $sideBarDimCover.classList.remove('hidden');
   });
   $closeSideBarBtn.addEventListener('click', () => {
     if ($sideBar.classList.contains('open_side_bar')) $sideBar.classList.remove('open_side_bar');
     $sideBar.classList.add('close_side_bar');
-    $dimCover.classList.add('hidden');
+    $sideBarDimCover.classList.add('hidden');
   });
 
-  $dimCover.addEventListener('click', () => {
+  $sideBarDimCover.addEventListener('click', () => {
     if ($sideBar.classList.contains('open_side_bar')) $sideBar.classList.remove('open_side_bar');
     $sideBar.classList.add('close_side_bar');
-    $dimCover.classList.add('hidden');
+    $sideBarDimCover.classList.add('hidden');
   });
 
   const $expandedShoppingCategoryBtn = $('.expanded_shopping_category_btn', $sideBar);
