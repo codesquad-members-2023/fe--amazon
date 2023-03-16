5773ff2e3084f56b0c884768f4c5e28bb003e5d0
NANII <breadnanii@gmail.com>
2023-03-16 23:19:04 +0900

    ♻️ feat : sideBar.js 전역변수 -> 함수안으로 옮김
    - 큰 함수 작게 쪼갤 필요있음
    - 중복 최소화할 필요있음

--- javascript/sideBar.js
+++ javascript/sideBar.js
@@ -1,41 +1,53 @@
-const $every = document.querySelector('.every');
-const $close = document.getElementById('side_bar_close');
-const $side_bar = document.querySelector('.side_bar');
-const $view_all = document.getElementById('view_all');
-const $view_simple = document.getElementById('view_simple');
-const $fold_content = document.querySelector('.fold_content');
-const $fold_detail = document.querySelector('.fold_detail');
-const $side_container = document.querySelector('.side_container');
-const $back_btn = document.getElementById('back_btn');
-
-
-$every.addEventListener('click', e => {
+const sideBarEventHandler = () => {
+  const $every = document.querySelector('.every');
+  const $close = document.getElementById('side_bar_close');
   const $modal_bg = document.querySelector('.modal_background');
+  const $view_all = document.getElementById('view_all');
+  const $view_simple = document.getElementById('view_simple');
+  const $side_container = document.querySelector('.side_container');
+  const $back_btn = document.getElementById('back_btn');
+
+  $every.addEventListener('click', openSideBar);
+  $close.addEventListener('click', closeSideBar);
+  $modal_bg.addEventListener('click', closeSideBar);
+  $view_all.addEventListener('click', clickViewAll);
+  $view_simple.addEventListener('click', clickViewSimple);
+  $side_container.addEventListener('click', insertDetailDataHandler);
+  $back_btn.addEventListener('click', clickBackBtnHandler);
+}
+
+// 사이드바 여는 함수
+const openSideBar = e => {
+  const $side_bar = document.querySelector('.side_bar');
+  const $modal_bg = document.querySelector('.modal_background');
+
   $side_bar.classList.add('slideRight');
   $side_bar.classList.remove('slideLeft');
   $side_bar.style.display = 'flex';
   $modal_bg.classList.remove('hidden');
   $modal_bg.style.zIndex = '1';
-});
+}
 
-$close.addEventListener('click', e => {
+// 사이드바 닫는 함수
+const closeSideBar = e => {
+  const $side_bar = document.querySelector('.side_bar');
+  const $modal_bg = document.querySelector('.modal_background');
+
   $side_bar.classList.add('slideLeft');
-});
+  $side_bar.addEventListener('animationend', ({animationName, target}) => {
+    if(animationName === 'slideLeft' && target.className.includes('side_bar')) {
+      $side_bar.style.display = 'none';
+      $side_bar.classList.remove('slideRight');
+      $modal_bg.classList.add('hidden');
+      $modal_bg.style.zIndex = 'inherit';
+    }
+  });
+}
 
-$modal_bg.addEventListener('click', e => {
-  $side_bar.classList.add('slideLeft');
-});
+const clickViewAll = e => {
+  const $fold_content = document.querySelector('.fold_content');
+  const $view_all = document.getElementById('view_all');
 
-$side_bar.addEventListener('animationend', ({animationName, target}) => {
-  if(animationName === 'slideLeft' && target.className.includes('side_bar')) {
-    $side_bar.style.display = 'none';
-    $side_bar.classList.remove('slideRight');
-    $modal_bg.classList.add('hidden');
-    $modal_bg.style.zIndex = 'inherit';
-  }
-});
-
-$view_all.addEventListener('click',e => {
   if ($fold_content.style.maxHeight){
     $fold_content.style.maxHeight = null;
     $view_all.lastElementChild.outerHTML = "<img src=\"/asset/images/vectors/Bottom.svg\">"
@@ -43,14 +55,16 @@ $view_all.addEventListener('click',e => {
     $view_all.lastElementChild.outerHTML = "<img src=\"/asset/images/vectors/Up.svg\">"
     $fold_content.style.maxHeight = $fold_content.scrollHeight + "px";
   }
-});
+}
 
-$view_simple.addEventListener('click', e => {
+const clickViewSimple = e => {
+  const $fold_content = document.querySelector('.fold_content');
+  const $view_all = document.getElementById('view_all');
   $fold_content.style.maxHeight = null;
   $view_all.lastElementChild.outerHTML = "<img src=\"/asset/images/vectors/Bottom.svg\">"
-});
+}
 
-$side_container.addEventListener('click', e => {
+const insertDetailDataHandler = e => {
   const target = e.target;
   const isLI = target.closest('li');
   const isId = target.id !== 'view_all' && target.id !== 'view_simple';
@@ -58,28 +72,34 @@ $side_container.addEventListener('click', e => {
   if(isLI === null) return;
   const title = isLI.innerText;
   const isTitle = SIDEBAR_DETAIL.hasOwnProperty(title);
+
   if(isTitle && isId && isLI.tagName === 'LI') {
     const $tab = document.querySelector('.tab_content');
+    const $fold_detail = document.querySelector('.fold_detail');
     if ($tab.hasChildNodes()) $tab.replaceChildren();
+
     const data = SIDEBAR_DETAIL[title];
     const text = data.reduce((acc, cur) => {
       acc += `<li>${cur}</li>`;
       return acc;
     }, `<h3>${title}</h3><ul>`) + `</ul>` ;
+
     $tab.insertAdjacentHTML('beforeend', text);
     $fold_detail.classList.remove('slideLeft');
     $fold_detail.classList.add('slideRight');
     $fold_detail.classList.remove('hidden');
   }
-});
+}
 
-$back_btn.addEventListener('click', e => {
+const clickBackBtnHandler = e => {
+  const $fold_detail = document.querySelector('.fold_detail');
   $fold_detail.classList.remove('slideRight');
   $fold_detail.classList.add('slideLeft');
-});
+  $fold_detail.addEventListener('animationend', ({animationName, target}) => {
+    if(animationName === 'slideLeft' && target.className.includes('fold_detail')) {
+      $fold_detail.classList.add('hidden');
+    }
+  });
+}
 
-$fold_detail.addEventListener('animationend', e => {
-  if(e.animationName === 'slideLeft' && e.target.className.includes('fold_detail')) {
-    $fold_detail.classList.add('hidden');
-  }
-});
+sideBarEventHandler();
