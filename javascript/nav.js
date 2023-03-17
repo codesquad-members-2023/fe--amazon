807c3db3a591e831c815f2c75f8249203bf4cf16
NANII <breadnanii@gmail.com>
2023-03-17 16:37:39 +0900

    🐛 fix : nav bar toggle 메서드 변경
    - toggle 사용시 떠있으면 닫고, 닫혀있으면 팝업이 올라오기 때문에
     배송처에 마우스는 엔터해도 팝업이 자꾸 닫히는 버그가 발생하여 다시 함수 분리

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,14 +1,15 @@
 const navBarEventHandler = () => {
   const $header = document.querySelector('header');
   const $login = document.querySelector('.login');
-  const $shipping_address = document.querySelector('.address');
+  const $shipping_address_container = document.querySelector('.address_container');
   const $shipping_address_popup = document.querySelector('.address_popup');
   const $login_popup_detail = document.querySelector('.login_popup_detail');
 
   $header.addEventListener('mouseenter', hidePopup);
   $login.addEventListener('mouseenter', loginModalHandler);
-  $shipping_address.addEventListener('mouseenter', shippingAddressModalHandler);
-  $shipping_address_popup.addEventListener('mouseleave', shippingAddressModalHandler);
+  $shipping_address_container.addEventListener('mouseenter', popUpShippingAddressModalHandler);
+  $shipping_address_container.addEventListener('mouseleave', hideShippingAddressModalHandler);
+  $shipping_address_popup.addEventListener('mouseleave', hideShippingAddressModalHandler);
   $login_popup_detail.addEventListener('mouseleave', loginModalHandler);
 }
 
@@ -17,11 +18,18 @@ const hidePopup = () => {
   $fade_in_popup.classList.add('hidden');
 }
 
-const shippingAddressModalHandler = () => {
+const popUpShippingAddressModalHandler = () => {
   const $shipping_address_popup = document.querySelector('.address_popup');
   const $modal_bg = document.querySelector('.modal_background');
-  $shipping_address_popup.classList.toggle('hidden');
-  $modal_bg.classList.toggle('hidden');
+  $shipping_address_popup.classList.remove('hidden');
+  $modal_bg.classList.remove('hidden');
+}
+
+const hideShippingAddressModalHandler = () => {
+  const $shipping_address_popup = document.querySelector('.address_popup');
+  const $modal_bg = document.querySelector('.modal_background');
+  $shipping_address_popup.classList.add('hidden');
+  $modal_bg.classList.add('hidden');
 }
 
 const loginModalHandler = () => {
