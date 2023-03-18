d5aafdbb90f0e0f41ba67b52536edfcf262709a3
sarang_daddy <sarang_daddy@naver.com>
2023-03-19 00:31:54 +0900

    사이드바 디테일에서 클릭 이벤트시 발생하는 에러 해결
    - event target이 로직에서 존재하지 않는 요소를 클릭시 에러 발생
    - 로직에서 존재하지 않는 요소를 클릭시 예외처리 해줌

--- js/closeSidebarDetail.js
+++ js/closeSidebarDetail.js
@@ -3,23 +3,25 @@ const closeSidebarDetail = () => {
   const sidebarlist = document.querySelector(".homeSidebar__lists");
 
   const selectListInSidebarDeail = (event) => {
-    const backButtons = document.querySelectorAll(".homeSidebar__mode--back");
-    const seltedBtn = [...backButtons].find((button) => {
-      return button.contains(event.target);
-    });
-
     const thisElement = event.target.closest("ul");
-
     const liEventTarget = event.target.closest("li");
-    console.log(liEventTarget);
 
-    if (liEventTarget.className === seltedBtn.className) {
-      thisElement.classList.add("hideSidebarDeatilSoft");
-      thisElement.classList.remove("showDetail", "showSidebarDetailAnimation");
-      sidebarlist.classList.remove("none");
-    }
+    if (liEventTarget) {
+      const backButtons = document.querySelectorAll(".homeSidebar__mode--back");
+      const seltedBtn = [...backButtons].find((button) => {
+        return button.contains(event.target);
+      });
+
+      if (seltedBtn && liEventTarget.className === seltedBtn.className) {
+        thisElement.classList.add("hideSidebarDeatilSoft");
+        thisElement.classList.remove(
+          "showDetail",
+          "showSidebarDetailAnimation"
+        );
+        sidebarlist.classList.remove("none");
+      }
+    } else return;
   };
-
   sidebar.addEventListener("click", selectListInSidebarDeail);
 };
 
