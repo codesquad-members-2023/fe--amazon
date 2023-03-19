00253fd6302c672043c024772e0523942b7aa6d0
poco <godiva140@gmail.com>
2023-03-19 19:37:20 +0900

    refactor: sidebar extra list 요소 className 변경
    sidebar_list_unfold -> sidebar_extra_list_unfold

--- src/js/sidebarElement.js
+++ src/js/sidebarElement.js
@@ -4,10 +4,10 @@ const sidebar = {
   InactivateButton: document.querySelector('.sidebar_close_icon'),
   Lists: document.querySelectorAll('.sidebar_lists'),
   Main: document.querySelector('.sidebar_main'),
-  showExtraListButton: document.querySelector('.sidebar_list_unfold'),
+  showExtraListButton: document.querySelector('.sidebar_extra_list_unfold'),
   extraList: document.querySelector('.sidebar_extra_list_items'),
   extraListItem: document.querySelectorAll('.sidebar_extra_list_item'),
-  hideExtraListButton: document.querySelector('.sidebar_list_fold'),
+  hideExtraListButton: document.querySelector('.sidebar_extra_list_fold'),
 };
 
 export { sidebar };
