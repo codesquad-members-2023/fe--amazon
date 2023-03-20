9758a07f0d2f3ece8692717ea72c72723259e978
saseung <seung0122@naver.com>
2023-03-20 12:05:31 +0900

    ♻️ refactor: html, css, js 전체적인 코드 리팩토링
    - px에서 rem으로 단위 변경
    - css 불필요한 속성 수정
    - js classList 적용

--- js/navigation.js
+++ js/navigation.js
@@ -1,55 +1,14 @@
-function showModalAfterTimeout() {
-  setTimeout(() => {
-    const loginModal = document.querySelector(".login-modal");
-    loginModal.classList.add("visible");
-  }, 1000);
-}
+setTimeout(() => {
+  const el = document.querySelector(".login__modal-fadeIn");
+  el.classList.add("visible");
+}, 1000);
 
 function appendLoginHoverList() {
-  const loginHoverList = document.querySelector(".hover-list1");
-  const loginHoverAccount = document.querySelector(".hover-list2");
+  const $loginHoverList = document.querySelector(".list__items");
+  const $loginHoverAccount = document.querySelector(".accounts__items");
 
-  loginListItems.forEach((el) => loginHoverList.insertAdjacentHTML("beforeend", `<li>${el}</li>`));
-  accountMenuItems.forEach((el) => loginHoverAccount.insertAdjacentHTML("beforeend", `<li>${el}</li>`));
+  loginListItems.forEach((el) => $loginHoverList.insertAdjacentHTML("beforeend", `<li>${el}</li>`));
+  accountMenuItems.forEach((el) => $loginHoverAccount.insertAdjacentHTML("beforeend", `<li>${el}</li>`));
 }
 
-function loginMenuHover() {
-  const loginMenu = document.querySelector(".login-menu");
-  const loginHover = document.querySelector(".login-hover");
-  const dimmed = document.querySelector(".dimmed");
-  const loginModal = document.querySelector(".login-modal");
 
-  loginMenu.addEventListener("mouseover", () => {
-    loginHover.style.display = "block";
-    dimmed.style.display = "block";
-  });
-
-  loginMenu.addEventListener("mouseout", () => {
-    loginHover.style.display = "none";
-    loginModal.style.display = "none";
-    dimmed.style.display = "none";
-  });
-}
-
-function shippingHover() {
-  const shippingHover = document.querySelector(".shipping-hover");
-  const shippingCountry = document.querySelector(".shipping-country");
-  const dimmed = document.querySelector(".dimmed");
-  const loginModal = document.querySelector(".login-modal");
-
-  shippingCountry.addEventListener("mouseover", () => {
-    shippingHover.style.display = "block";
-    dimmed.style.display = "block";
-    loginModal.style.display = "none";
-  });
-
-  shippingCountry.addEventListener("mouseout", () => {
-    shippingHover.style.display = "none";
-    dimmed.style.display = "none";
-  });
-}
-
-appendLoginHoverList();
-showModalAfterTimeout();
-loginMenuHover();
-shippingHover();
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,50 +1,24 @@
-function viewSidebar() {
-  const sidebarShowButton = document.querySelector(".nav-sidebar-button");
-  const sidebar = document.querySelector(".sidebar");
-  const closeButton = document.querySelector(".sidebar-close-button");
-  const dimmed = document.querySelector(".dimmed");
-  const loginModal = document.querySelector(".login-modal");
-  const sidebarOtherContents = document.querySelector(".sidebar-other-contents");
-  const quickView = document.querySelector(".quick-view");
+function sideBarActive() {
+  const $viewSidebarButton = document.querySelector(".sidebar__button-view");
+  const $sideBar = document.querySelector(".sidebar");
+  const $closeSidebarButton = document.querySelector(".sidebar__button-close");
 
+  viewSideBar($viewSidebarButton, $sideBar, $closeSidebarButton);
+  closeSideBar($closeSidebarButton, $sideBar);
+}
 
-  sidebarShowButton.addEventListener("click", () => {
-    sidebarHiddenItems.forEach((el) => sidebarOtherContents.insertAdjacentHTML("beforeend", `<li><div>${el}</div><img src="./assets/arrow.svg" /></li>`));
-    quickView.insertAdjacentHTML("afterbegin", `<li><div>간단히 보기</div><img src="./assets/arrow.svg" /></li>`);
-    sidebar.style.left = "0";
-    closeButton.style.opacity = "100%";
-    dimmed.style.display = "block";
-    loginModal.style.display = "none";
+function viewSideBar(viewButton, sideBar, closeButton) {
+  viewButton.addEventListener("click", () => {
+    sideBar.classList.add("sidebar__active");
+    closeButton.classList.add("button__close-view");
   });
 }
 
-function hideSidebar() {
-  const sidebarCloseButton = document.querySelector(".sidebar-close-button");
-  const sidebar = document.querySelector(".sidebar");
-  const dimmed = document.querySelector(".dimmed");
-
-  sidebarCloseButton.addEventListener("click", () => {
-    sidebar.style.left = "-400px";
-    sidebarCloseButton.style.opacity = "0%";
-    dimmed.style.display = "none";
+function closeSideBar(closeButton, sideBar) {
+  closeButton.addEventListener("click", () => {
+    sideBar.classList.remove("sidebar__active");
+    closeButton.classList.remove("button__close-view");
   });
 }
 
-function openCategoryMenu() {
-  const sidebarView = document.querySelector(".view-all-contents");
-  const sidebarOtherContents = document.querySelector(".sidebar-other-contents");
-
-  sidebarView.addEventListener("click", () => (sidebarOtherContents.style.height = "100%"));
-}
-
-function closeCategoryMenu() {
-  const sidebarQuickView = document.querySelector(".quick-view");
-  const sidebarOtherContents = document.querySelector(".sidebar-other-contents");
-
-  sidebarQuickView.addEventListener("click", () => (sidebarOtherContents.style.height = "0"));
-}
-
-viewSidebar();
-hideSidebar();
-openCategoryMenu();
-closeCategoryMenu();
+sideBarActive();
