90350a241906ec7838b284e2146e35ae74fb415b
안예림 <an-yelim@an-yelim-ui-MacBookAir.local>
2023-03-20 15:52:41 +0900

    refactor: 파일 내에서 각 기능 별로 분리
    파일 내에서 세분화적으로 각 기능을 분리하였습니다.

--- scripts/all/allJs.js
+++ scripts/all/allJs.js
@@ -1,5 +1,5 @@
-import "/Users/an-yelim/codesquad/fe--amazon/scripts/nav/navDimmed.js";
-import "/Users/an-yelim/codesquad/fe--amazon/scripts/sidebar/sidebarDimmed.js";
+import "../nav/dimmedNav.js";
+import "../sidebar/dimmedSidebar.js";
 import "/Users/an-yelim/codesquad/fe--amazon/scripts/sidebar/showSidebar.js";
 import "/Users/an-yelim/codesquad/fe--amazon/scripts/sidebar/sidebarInputData.js";
 import "/Users/an-yelim/codesquad/fe--amazon/scripts/all/allQuery.js";
--- scripts/all/allQuery.js
+++ scripts/all/allQuery.js
@@ -1,12 +1,5 @@
-const ALL_SELECTORS = {
-  dimmed: document.querySelector(".dim-layer"),
-  sidebarAll: document.querySelector(".sub__all-menu"),
-  sidebarClose: document.querySelector(".sidebar__btn_close"),
-  loginMenu: document.querySelector(".nav__login_popup-box"),
-  shippingMenu: document.querySelector(".nav__shipping"),
-  sidebar: document.querySelector(".sidebar"),
-  menuAll: document.querySelector(".sidebar__menu-all"),
-  menuAllDetail: document.querySelector(".sidebar__all-menu_extend"),
-};
+const $ = (selector, parent = document) => parent.querySelector(selector);
+const $$ = (selector, parent = document) => parent.querySelectorAll(selector);
 
-export { ALL_SELECTORS };
+export { $, $$ };
+
