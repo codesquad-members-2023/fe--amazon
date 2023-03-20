93ce04b83887bc597615d006f77ab9245a9bfecf
JangHoonSeok <orirori512@naver.com>
2023-03-20 23:27:56 +0900

    ✨ feat: 캐러셀 초기 구조 완성

--- js/carousel.js
+++ js/carousel.js
@@ -1,1 +1,31 @@
-class Carousel {}
+class Carousel {
+  constructor() {
+    this.leftButton = document.querySelector('.carousel_left');
+    this.rightButton = document.querySelector('.carousel_right');
+    this.carouselWindow = document.querySelector('.carousel_window');
+    this.IMG_COUNT = 5;
+  }
+  init() {
+    this.addBtnEvents();
+    this.createCells();
+  }
+
+  addBtnEvents() {
+    this.leftButton.addEventListener('click', this.leftBtnClickEvent());
+    this.rightButton.addEventListener('click', this.rightBtnClickEvent());
+  }
+  createCells() {
+    const container = this.carouselWindow.querySelector('.carousel_container');
+    const imgArr = Array.from(
+      { length: this.IMG_COUNT },
+      (_, i) => `../asset/main_image${i + 1}`,
+    );
+    console.log(imgArr);
+  }
+  leftBtnClickEvent() {}
+  rightBtnClickEvent() {}
+}
+
+const carousel = new Carousel();
+carousel.init();
+console.log(carousel.leftButton);
