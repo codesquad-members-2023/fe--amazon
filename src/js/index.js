06984954b21a02230923759dd0f91d0aceeed608
yousooa <kinsk2839@naver.com>
2023-03-20 14:04:10 +0900

    refactor: 변수명 변경 및 리뷰 반영
    - forEach를 이용해 중복되는 부분 줄이기

--- src/js/index.js
+++ src/js/index.js
@@ -3,83 +3,75 @@ import { SIDEBAR_DETAIL } from './constant/sideBarDetail.js';
 
 const main = () => {
   const $sideBar = $('.side-bar');
-  const $expandedShoppingCategoryList = $('.shopping-category__expanded-list', $sideBar);
+  const $expandedCategory = $('.shopping-category__expanded-list', $sideBar);
   const SIDEBAR_CATEGORY = Object.keys(SIDEBAR_DETAIL);
   const carIdx = SIDEBAR_CATEGORY.indexOf('자동차 용품');
   const EXPANDED_CATEGORY = SIDEBAR_CATEGORY.slice(carIdx);
   const expandedCategoryTemplate = EXPANDED_CATEGORY.reduce(
     (template, category) =>
-      template +
-      String.raw`
-      <li>${category}<span class="right-arrow-icon"></span></li>`,
-    '',
+      template + String.raw`<li>${category}<span class="right-arrow-icon"></span></li>`,
+    ''
   );
 
-  $expandedShoppingCategoryList.insertAdjacentHTML('afterbegin', expandedCategoryTemplate);
+  $expandedCategory.insertAdjacentHTML('afterbegin', expandedCategoryTemplate);
 
   const $headerDimCover = $('.dim-cover__in-header');
 
-  const $shippingAddress = $('.nav-main__shipping-address');
+  const $navMain = $('#nav-main');
+  const $shippingAddress = $('.nav-main__shipping-address', $navMain);
   const $shippingAddressModal = $('.shipping-address__modal', $shippingAddress);
 
   $shippingAddress.addEventListener('mouseenter', () => {
-    $shippingAddressModal.classList.remove('hidden');
-    $headerDimCover.classList.remove('hidden');
+    [$shippingAddressModal, $headerDimCover].forEach((ele) => ele.classList.remove('hidden'));
   });
   $shippingAddress.addEventListener('mouseleave', () => {
-    $shippingAddressModal.classList.add('hidden');
-    $headerDimCover.classList.add('hidden');
+    [$shippingAddressModal, $headerDimCover].forEach((ele) => ele.classList.add('hidden'));
   });
 
-  const $login = $('.nav-main__login');
+  const $login = $('.nav-main__login', $navMain);
   const $loginModal = $('.login__modal', $login);
 
   $login.addEventListener('mouseenter', () => {
-    $loginModal.classList.remove('hidden');
-    $headerDimCover.classList.remove('hidden');
+    [$loginModal, $headerDimCover].forEach((ele) => ele.classList.remove('hidden'));
   });
   $login.addEventListener('mouseleave', () => {
-    $loginModal.classList.add('hidden');
-    $headerDimCover.classList.add('hidden');
+    [$loginModal, $headerDimCover].forEach((ele) => ele.classList.add('hidden'));
   });
 
-  const $sideBarBtn = $('.side-bar-btn');
+  const $navSub = $('#nav-sub');
+  const $sideBarBtn = $('.side-bar-btn', $navSub);
   const $closeSideBarBtn = $('.side-bar__close-btn', $sideBar);
   const $sideBarDimCover = $('.dim-cover__in-sideBar');
 
   $sideBarBtn.addEventListener('click', () => {
-    if ($sideBar.classList.contains('close_side-bar')) $sideBar.classList.remove('close_side-bar');
-    $sideBar.classList.add('open_side-bar');
+    $sideBar.classList.add('open');
     $sideBarDimCover.classList.remove('hidden');
   });
   $closeSideBarBtn.addEventListener('click', () => {
-    if ($sideBar.classList.contains('open_side-bar')) $sideBar.classList.remove('open_side-bar');
-    $sideBar.classList.add('close_side-bar');
+    $sideBar.classList.remove('open');
     $sideBarDimCover.classList.add('hidden');
   });
-
   $sideBarDimCover.addEventListener('click', () => {
-    if ($sideBar.classList.contains('open_side-bar')) $sideBar.classList.remove('open_side-bar');
-    $sideBar.classList.add('close_side-bar');
+    $sideBar.classList.remove('open');
     $sideBarDimCover.classList.add('hidden');
   });
 
-  const $expandedShoppingCategoryBtn = $('.shopping-category__expanded-btn', $sideBar);
-  $expandedShoppingCategoryBtn.addEventListener('click', () => {
-    if ($expandedShoppingCategoryList.maxHeight) {
-      $expandedShoppingCategoryList.style.maxHeight = null;
+  const $expandedCategoryBtn = $('.shopping-category__expanded-btn', $sideBar);
+  $expandedCategoryBtn.addEventListener('click', () => {
+    if ($expandedCategory.maxHeight) {
+      $expandedCategory.style.maxHeight = null;
       return;
     }
-    $expandedShoppingCategoryList.style.maxHeight = `${$expandedShoppingCategoryList.scrollHeight}px`;
+    $expandedCategory.style.maxHeight = `${$expandedCategory.scrollHeight}px`;
   });
 
-  const $closeExpandedShoppingCategory = $('.shopping-category__close-expanded-btn', $sideBar);
-  $closeExpandedShoppingCategory.addEventListener('click', () => {
-    if (!$expandedShoppingCategoryList.maxHeight) {
-      $expandedShoppingCategoryList.style.maxHeight = null;
+  const $expandedCategoryCloseBtn = $('.shopping-category__close-expanded-btn', $sideBar);
+  $expandedCategoryCloseBtn.addEventListener('click', () => {
+    if (!$expandedCategory.maxHeight) {
+      $expandedCategory.style.maxHeight = null;
       return;
     }
-    $expandedShoppingCategoryList.style.maxHeight = `${$expandedShoppingCategoryList.scrollHeight}px`;
+    $expandedCategory.style.maxHeight = `${$expandedCategory.scrollHeight}px`;
   });
 };
 
