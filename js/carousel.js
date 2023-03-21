d36d0d137c40060b3f8083833cf15ae8b5129044
JangHoonSeok <orirori512@naver.com>
2023-03-21 17:50:23 +0900

    ✨ feat: 캐러셀 완료.

--- js/carousel.js
+++ js/carousel.js
@@ -3,28 +3,32 @@ class Carousel {
     this.leftButton = document.querySelector('.carousel_left');
     this.rightButton = document.querySelector('.carousel_right');
     this.carouselWindow = document.querySelector('.carousel_window');
+    this.container = this.carouselWindow.querySelector('.carousel_container');
     this.IMG_COUNT = 5; // 배경 이미지 개수만 변경하면 됨.
   }
+
   init() {
-    this.addBtnEvents();
     this.createCells();
+    this.addBtnEvents();
   }
 
   addBtnEvents() {
-    this.leftButton.addEventListener('click', this.leftBtnClickEvent());
-    this.rightButton.addEventListener('click', this.rightBtnClickEvent());
+    this.leftButton.addEventListener('click', () => {
+      this.leftBtnClickEvent(0);
+    });
+    this.rightButton.addEventListener('click', () => {
+      this.rightBtnClickEvent(1);
+    });
   }
 
   createCells() {
-    const container = this.carouselWindow.querySelector('.carousel_container');
-
     const imgSrcArr = Array.from(
       { length: this.IMG_COUNT },
       (_, i) => `../../asset/main_image${i + 1}.jpg`,
     );
 
     imgSrcArr.map((imgSrc) => {
-      container.appendChild(this.createCellHTML(imgSrc));
+      this.container.appendChild(this.createCellHTML(imgSrc));
     });
   }
 
@@ -35,10 +39,32 @@ class Carousel {
     return cellHTML;
   }
 
-  leftBtnClickEvent() {}
-  rightBtnClickEvent() {}
+  leftBtnClickEvent(direction) {
+    this.container.style.transitionDuration = '500ms';
+    this.container.style.transform = `translateX(${100 / this.IMG_COUNT}%)`;
+    this.container.ontransitionend = () => {
+      this.controlChildNodes(direction);
+    };
+  }
+  rightBtnClickEvent(direction) {
+    this.container.style.transitionDuration = '500ms';
+    this.container.style.transform = `translateX(${
+      (100 / this.IMG_COUNT) * -1
+    }%)`;
+    this.container.ontransitionend = () => {
+      this.controlChildNodes(direction);
+    };
+  }
+  controlChildNodes(direction) {
+    this.container.removeAttribute('style');
+    direction === 1
+      ? this.container.appendChild(this.container.firstElementChild)
+      : this.container.insertBefore(
+          this.container.lastElementChild,
+          this.container.firstElementChild,
+        );
+  }
 }
 
 const carousel = new Carousel();
 carousel.init();
-console.log(carousel.leftButton);
