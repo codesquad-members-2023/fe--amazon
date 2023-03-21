9093f1c0b6d4266915f64bc7f1b2ada190745dde
JangHoonSeok <orirori512@naver.com>
2023-03-21 10:09:37 +0900

    ✨ feat: 캐러셀 이미지 5개 받아오기 성공

--- js/carousel.js
+++ js/carousel.js
@@ -3,7 +3,7 @@ class Carousel {
     this.leftButton = document.querySelector('.carousel_left');
     this.rightButton = document.querySelector('.carousel_right');
     this.carouselWindow = document.querySelector('.carousel_window');
-    this.IMG_COUNT = 5;
+    this.IMG_COUNT = 5; // 배경 이미지 개수만 변경하면 됨.
   }
   init() {
     this.addBtnEvents();
@@ -14,14 +14,27 @@ class Carousel {
     this.leftButton.addEventListener('click', this.leftBtnClickEvent());
     this.rightButton.addEventListener('click', this.rightBtnClickEvent());
   }
+
   createCells() {
     const container = this.carouselWindow.querySelector('.carousel_container');
-    const imgArr = Array.from(
+
+    const imgSrcArr = Array.from(
       { length: this.IMG_COUNT },
-      (_, i) => `../asset/main_image${i + 1}`,
+      (_, i) => `../../asset/main_image${i + 1}.jpg`,
     );
-    console.log(imgArr);
+
+    imgSrcArr.map((imgSrc) => {
+      container.appendChild(this.createCellHTML(imgSrc));
+    });
   }
+
+  createCellHTML(imgSrc) {
+    let cellHTML = document.createElement('div');
+    cellHTML.setAttribute('class', 'carousel_img');
+    cellHTML.style.backgroundImage = `url(${imgSrc})`;
+    return cellHTML;
+  }
+
   leftBtnClickEvent() {}
   rightBtnClickEvent() {}
 }
