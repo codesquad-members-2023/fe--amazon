bc5005ce2fb8028ba44ccc3132032ad43411873d
saseung <seung0122@naver.com>
2023-03-23 00:50:21 +0900

    ♻️ refactor: 딤처리 효과 개선
    - display:none->opacity:0으로 수정
    - z-index 변수 지정
    - 사이드바, 모달창에 대한 딤처리 완료
    - 사이드바 애니메이션 효과 시간 수정

--- js/layout/sidebar.js
+++ js/layout/sidebar.js
@@ -1,24 +1,34 @@
-import { $ } from "../utils.js"
+import { $ } from "../utils.js";
 
 class Sidebar {
   constructor() {
-    this.sideBar = $(".sidebar");
+    this.sidebar = $(".sidebar");
     this.openSidebarButton = $(".sidebar__button-open");
     this.closeSidebarButton = $(".sidebar__button-close");
+    this.dimmedLayer = $(".dimmed-layer");
   }
 
   openSidebar() {
     this.openSidebarButton.addEventListener("click", () => {
-      this.sideBar.classList.add("sidebar__active");
+      this.sidebar.classList.add("sidebar__active");
       this.closeSidebarButton.classList.add("button__close-show");
+      this.dimmedLayer.classList.add("dimmed-layer__show");
+      this.dimmedLayer.classList.add("dimmed-layer__zindex");
     });
   }
 
   closeSidebar() {
     this.closeSidebarButton.addEventListener("click", () => {
-      this.sideBar.classList.remove("sidebar__active");
+      this.sidebar.classList.remove("sidebar__active");
       this.closeSidebarButton.classList.remove("button__close-show");
+      this.dimmedLayer.classList.remove("dimmed-layer__show");
+      this.dimmedLayer.classList.remove("dimmed-layer__zindex");
     });
+  }
+
+  runSidebar() {
+    this.openSidebar();
+    this.closeSidebar();
   }
 }
 
