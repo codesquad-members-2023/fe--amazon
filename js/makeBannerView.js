da023ae1f10906a4f5c6f155831e7c1b9b853288
sarang_daddy <sarang_daddy@naver.com>
2023-03-22 10:40:14 +0900

    배너 이미지 생성 (프로토타입 사용)
    - HTML에 배너 section생성
    - js의 생성자 객체를 활용해서 배너 생성

---
+++ js/makeBannerView.js
@@ -0,0 +1,27 @@
+const bannerSection = document.querySelector(".banner");
+
+const bannerImgArray = [
+  "bannerImg1",
+  "bannerImg2",
+  "bannerImg3",
+  "bannerImg4",
+  "bannerImg5",
+  "bannerImg6",
+];
+
+function Banner(name) {
+  this.name = name; // name 초기화
+}
+
+Banner.prototype.toBeElement = function () {
+  const div = document.createElement("div");
+  const img = document.createElement("img");
+  div.classList.add("banner__column");
+  img.classList.add("banner__img");
+  img.src = `photo/${this.name}.jpg`;
+  div.appendChild(img);
+  bannerSection.appendChild(div);
+};
+
+const bannerOne = new Banner("bannerImg1");
+bannerOne.toBeElement();
