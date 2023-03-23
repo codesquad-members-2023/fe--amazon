fb821749c13343e1f6322d22ac7a2dfe7c82f94e
NANII <breadnanii@gmail.com>
2023-03-23 11:40:28 +0900

    ♻️ Refactor : Carousel position CSS 코드수정

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -1,13 +1,13 @@
 const setImg = () => {
   const CAROUSELCOUNT = 6;
-  const directoryPath = '/asset/images/carousel';
   const imageFiles = Array.from({ length : CAROUSELCOUNT }, (_, i) => i);
 
-  let imgTemplate = '';
-  imageFiles.forEach(number => {
+  const imgTemplate = imageFiles.reduce((acc, number) => {
+    const directoryPath = '/asset/images/carousel';
     const img = `<img src="${directoryPath}/${number}.jpg" alt="carousel 배경이미지">`;
-    imgTemplate += img;
-  });
+    acc += img;
+    return acc;
+  }, '');
 
   const $carousel_container = document.querySelector('.image_box');
   $carousel_container.insertAdjacentHTML('afterbegin', imgTemplate);
