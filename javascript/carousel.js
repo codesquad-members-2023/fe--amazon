4f73d3996bd63aa1d65c517200d387a5177f024d
NANII <breadnanii@gmail.com>
2023-03-23 23:43:49 +0900

    🎨 feat : Carousel Class로 코드 구조 변경

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -1,16 +1,43 @@
-const setCarouselImage = () => {
-  const CAROUSELCOUNT = 6;
-  const imageFiles = Array.from({ length : CAROUSELCOUNT }, (_, i) => i);
+class Carousel {
+  constructor(count) {
+    this.CAROUSELCOUNT = count;
+    this.directoryPath = '/asset/images/carousel';
+    this.slider = document.querySelector('.slider');
+    this.slides = document.querySelectorAll('.slide');
+    this.leftBtn = document.getElementById('carousel_left');
+    this.rightBtn = document.getElementById('carousel_right');
+  }
 
-  const imgTemplate = imageFiles.reduce((acc, number) => {
-    const directoryPath = '/asset/images/carousel';
-    const img = `<img src="${directoryPath}/${number}.jpg" alt="carousel 배경이미지">`;
-    acc += img;
-    return acc;
-  }, '');
+  init() {
+    this.setImage();
+    this.slideHandler();
+    this.slideAuto();
+  }
 
-  const $carousel_container = document.querySelector('.image_box');
-  $carousel_container.insertAdjacentHTML('afterbegin', imgTemplate);
+  setImage() {
+    const imageFiles = Array.from({ length : this.CAROUSELCOUNT }, (_, i) => i);
+    const imageTemplate = imageFiles.reduce((acc, number) => {
+      const img = `<img class="slide" src="${this.directoryPath}/${number}.jpg" alt="carousel 배경이미지${number}">`;
+      acc += img;
+      return acc;
+    }, '');
+    this.slider.insertAdjacentHTML('afterbegin', imageTemplate);
+  }
+
+  slideHandler() {
+    this.leftBtn.addEventListener('click', this.translateSlide.bind(this));
+    this.rightBtn.addEventListener('click', this.translateSlide.bind(this));
+  }
+
+  translateSlide({ target }) {
+    const spanId = target.closest('span').id;
+    this.slider.style.transitionDuration = '500ms';
+    this.slider.style.transform = `translate3d(-50%, 0, 0)`;
+  }
+
+  slideAuto() {
+    
+  }
 }
 
-export { setCarouselImage };
+export { Carousel };
--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,13 @@
 import { sideBarEventHandler } from './sideBar.js';
 import { navBarEventHandler } from './nav.js';
-import { setCarouselImage } from './carousel.js';
+import { Carousel } from './carousel.js';
 import { insertLoginDetailData } from './loginDetail.js';
 
 const main = () => {
+  const CAROUSELCOUNT = 6;
   sideBarEventHandler();
   navBarEventHandler();
-  setCarouselImage();
+  new Carousel(CAROUSELCOUNT).init();
   insertLoginDetailData();
 }
 
