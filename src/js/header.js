36a57546fd3a3b56ac272ecaef2f2bb1a3cc193e
yousooa <kinsk2839@naver.com>
2023-03-24 00:25:59 +0900

    fix:header.js bind 삭제
    - bind를 사용하면서 this를 잘못가리키고 있는지 오류 발생
    
    - 추후에 bind로 고칠 예정

--- src/js/header.js
+++ src/js/header.js
@@ -12,36 +12,22 @@ export default class Header {
     this.$loginModal = $('.login__modal', this.$login);
   }
 
-  showModal(...elements) {
-    // [this.$shippingAddressModal, this.$headerDimCover].forEach((ele) =>
-    //   ele.classList.remove('hidden')
-    // );
-    [...elements].forEach((ele) => ele.classList.remove('hidden'));
-  }
-
-  hideModal(...elements) {
-    // [this.$shippingAddressModal, this.$headerDimCover].forEach((ele) =>
-    //   ele.classList.remove('hidden')
-    // );
-    [...elements].forEach((ele) => ele.classList.add('hidden'));
-  }
-
   addEvents() {
-    this.$shippingAddress.addEventListener(
-      'mouseenter',
-      this.showModal.bind(this, this.$shippingAddressModal, this.$headerDimCover)
-    );
-    this.$shippingAddress.addEventListener(
-      'mouseleave',
-      this.hideModal.bind(this, this.$shippingAddressModal, this.$headerDimCover)
-    );
-    this.$login.addEventListener(
-      'mouseenter',
-      this.showModal.bind(this, this.$loginModal, this.$headerDimCover)
-    );
-    this.$login.addEventListener(
-      'mouseleave',
-      this.hideModal.bind(this, this.$loginModal, this.$headerDimCover)
-    );
+    this.$shippingAddress.addEventListener('mouseenter', () => {
+      this.$shippingAddressModal.classList.remove('hidden');
+      this.$headerDimCover.classList.remove('hidden');
+    });
+    this.$shippingAddress.addEventListener('mouseleave', () => {
+      this.$shippingAddressModal.classList.add('hidden');
+      this.$headerDimCover.classList.add('hidden');
+    });
+    this.$login.addEventListener('mouseenter', () => {
+      this.$loginModal.classList.remove('hidden');
+      this.$headerDimCover.classList.remove('hidden');
+    });
+    this.$login.addEventListener('mouseleave', () => {
+      this.$loginModal.classList.add('hidden');
+      this.$headerDimCover.classList.add('hidden');
+    });
   }
 }
