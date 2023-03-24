85075e8692e5627b3d07e5a0b45aedd7776cd4d2
sarang_daddy <sarang_daddy@naver.com>
2023-03-24 15:17:43 +0900

    배너 캐러셀 코드 수정
    - 알지 못하고 사용한 bind 제거
    - 내가 아는 수준에서의 코드로 수정
    - 여기서 다시 업데이트 해보기

--- js/controlBannerView.js
+++ js/controlBannerView.js
@@ -3,24 +3,26 @@ const bannerList = document.querySelectorAll(".banner__column");
 const leftBtn = document.querySelector(".banner__btn:first-child");
 const rightBtn = document.querySelector(".banner__btn:last-child");
 
-(function addEvent() {
-  leftBtn.addEventListener("click", translateTrack.bind(this, 1));
-  rightBtn.addEventListener("click", translateTrack.bind(this, -1));
-})();
+let directionByNumber = 0;
 
-function translateTrack(direction) {
-  const selectedBtn = direction === 1 ? "left" : "right";
-  // bannerTrack.style.transitionDuration = "500ms";
-  bannerTrack.style.transition = "all 1s ease-in-out";
-  bannerTrack.style.transform = `translateX(${direction * (100 / 6)}%)`;
-  bannerTrack.ontransitionend = (e) => {
-    reorgannizeEl(selectedBtn);
+const moveBannerTrack = (direction) => {
+  if (direction === "left") {
+    directionByNumber = 1;
+  } else if (direction === "right") {
+    directionByNumber = -1;
+  }
+  bannerTrack.style.transition = "transform .5s ease-in-out";
+  bannerTrack.style.transform = `translateX(${
+    (directionByNumber * 100) / bannerList.length
+  }%)`;
+  bannerTrack.ontransitionend = () => {
+    reorgannizeEl(direction);
   };
-}
+};
 
-function reorgannizeEl(selectedBtn) {
+function reorgannizeEl(direction) {
   bannerTrack.removeAttribute("style");
-  selectedBtn === "left"
+  direction === "left"
     ? bannerTrack.insertBefore(
         bannerTrack.lastElementChild,
         bannerTrack.firstElementChild
@@ -28,28 +30,9 @@ function reorgannizeEl(selectedBtn) {
     : bannerTrack.appendChild(bannerTrack.firstElementChild);
 }
 
-// let currentBanner = 0;
-
-// const moveBannerTrack = (direction) => {
-//   if (direction === "left") {
-//     currentBanner++;
-//     if (currentBanner >= bannerList.length) {
-//       currentBanner = 0;
-//     }
-//   } else if (direction === "right") {
-//     currentBanner--;
-//     if (currentBanner < 0) {
-//       currentBanner = bannerList.length - 1;
-//     }
-//   }
-//   bannerTrack.style.transform = `translateX(-${
-//     (currentBanner * 100) / bannerList.length
-//   }%)`;
-// };
-
-// leftBtn.addEventListener("click", () => {
-//   moveBannerTrack("left");
-// });
-// rightBtn.addEventListener("click", () => {
-//   moveBannerTrack("right");
-// });
+leftBtn.addEventListener("click", function () {
+  moveBannerTrack("left");
+});
+rightBtn.addEventListener("click", function () {
+  moveBannerTrack("right");
+});
