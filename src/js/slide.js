1ea91d7db4ad11d6a8fd5cc4bc849938f0129a77
yousooa <kinsk2839@naver.com>
2023-03-27 18:23:53 +0900

    refactor: slide 변수명 저장하는 객체 생성 및 eventhandler 메소드 생성
    - 오른쪽 화살표 버튼과 왼쪽 화살표 버튼 eventhandler 메서드 생성

--- src/js/slide.js
+++ src/js/slide.js
@@ -7,19 +7,31 @@ export default class Slide {
 
   #startIdx = 0;
 
+  #slideSpeed = 300;
+
+  #SELECTOR = {
+    SLIDE: '.main-content__slide',
+    BOX: '.slide-box',
+    LIST: '.slide-list',
+    ITEM: '.slide-item',
+    PREV_BTN: '.slide-prev-btn',
+    NEXT_BTN: '.slide-next-btn',
+    ACTIVE: '.slide-active',
+  };
+
   constructor() {
-    this.$slide = $('.main-content__slide');
-    this.$slideBox = $('.slide-box', this.$slide);
-    this.$slideList = $('.slide-list', this.$slideBox);
-    this.$slideItems = $$('.slide-item', this.$slideList);
+    this.$slide = $(this.#SELECTOR.SLIDE);
+    this.$slideBox = $(this.#SELECTOR.BOX, this.$slide);
+    this.$slideList = $(this.#SELECTOR.LIST, this.$slideBox);
+    this.$slideItems = $$(this.#SELECTOR.ITEM, this.$slideList);
 
     this.currentIdx = this.#startIdx;
     this.$currentSlide = this.$slideItems[this.currentIdx];
 
     this.#itemCount = this.$slideItems.length;
 
-    this.$prevBtn = $('.slide-prev-btn', this.$slide);
-    this.$nextBtn = $('.slide-next-btn', this.$slide);
+    this.$prevBtn = $(this.#SELECTOR.PREV_BTN, this.$slide);
+    this.$nextBtn = $(this.#SELECTOR.NEXT_BTN, this.$slide);
 
     this.renderCopyItems();
   }
@@ -39,50 +51,53 @@ export default class Slide {
 
     this.$slideList.style.width = `${100 * totalItemCount}%`;
     this.$slideList.style.transform = `translate3d(-${this.#itemWidthUnit}%, 0, 0)`;
-    this.$currentSlide.classList.add('slide-active');
+    this.$currentSlide.classList.add(this.#SELECTOR.ACTIVE);
+  }
+
+  moveLeftHandler() {
+    if (this.currentIdx >= 0) {
+      this.$slideList.style.transition = `${this.#slideSpeed}ms`;
+      this.$slideList.style.transform = `translate3d(-${
+        this.#itemWidthUnit * this.currentIdx
+      }%, 0, 0)`;
+    }
+    if (this.currentIdx === 0) {
+      setTimeout(() => {
+        this.$slideList.style.transition = '0ms';
+        this.$slideList.style.transform = `translate3d(-${
+          this.#itemWidthUnit * this.#itemCount
+        }%, 0, 0)`;
+      }, this.#slideSpeed);
+      this.currentIdx = this.#itemCount;
+    }
+    this.$currentSlide.classList.remove(this.#SELECTOR.ACTIVE);
+    this.currentIdx -= 1;
+    this.$currentSlide = this.$slideItems[this.currentIdx];
+    this.$currentSlide.classList.add(this.#SELECTOR.ACTIVE);
+  }
+
+  moveRightHandler() {
+    if (this.currentIdx <= this.#itemCount - 1) {
+      this.$slideList.style.transition = `${this.#slideSpeed}ms`;
+      this.$slideList.style.transform = `translate3d(-${
+        this.#itemWidthUnit * (this.currentIdx + 2)
+      }%, 0, 0)`;
+    }
+    if (this.currentIdx === this.#itemCount - 1) {
+      setTimeout(() => {
+        this.$slideList.style.transition = '0ms';
+        this.$slideList.style.transform = `translate3d(-${this.#itemWidthUnit}%, 0, 0)`;
+      }, this.#slideSpeed);
+      this.currentIdx = -1;
+    }
+    this.$currentSlide.classList.remove(this.#SELECTOR.ACTIVE);
+    this.currentIdx += 1;
+    this.$currentSlide = this.$slideItems[this.currentIdx];
+    this.$currentSlide.classList.add(this.#SELECTOR.ACTIVE);
   }
 
   addEvents() {
-    this.$nextBtn.addEventListener('click', () => {
-      if (this.currentIdx <= this.#itemCount - 1) {
-        this.$slideList.style.transition = '300ms';
-        this.$slideList.style.transform = `translate3d(-${
-          this.#itemWidthUnit * (this.currentIdx + 2)
-        }%, 0, 0)`;
-      }
-      if (this.currentIdx === this.#itemCount - 1) {
-        setTimeout(() => {
-          this.$slideList.style.transition = '0ms';
-          this.$slideList.style.transform = `translate3d(-${this.#itemWidthUnit}%, 0, 0)`;
-        }, 300);
-        this.currentIdx = -1;
-      }
-      this.$currentSlide.classList.remove('slide-active');
-      this.currentIdx += 1;
-      this.$currentSlide = this.$slideItems[this.currentIdx];
-      this.$currentSlide.classList.add('slide-active');
-    });
-    this.$prevBtn.addEventListener('click', () => {
-      if (this.currentIdx >= 0) {
-        this.$slideList.style.transition = '300ms';
-        this.$slideList.style.transform = `translate3d(-${
-          this.#itemWidthUnit * this.currentIdx
-        }%, 0, 0)`;
-      }
-      if (this.currentIdx === 0) {
-        setTimeout(() => {
-          this.$slideList.style.transition = '0ms';
-          this.$slideList.style.transform = `translate3d(-${
-            this.#itemWidthUnit * this.#itemCount
-          }%, 0, 0)`;
-        }, 300);
-        this.currentIdx = this.#itemCount;
-      }
-
-      this.$currentSlide.classList.remove('slide-active');
-      this.currentIdx -= 1;
-      this.$currentSlide = this.$slideItems[this.currentIdx];
-      this.$currentSlide.classList.add('slide-active');
-    });
+    this.$prevBtn.addEventListener('click', this.moveLeftHandler.bind(this));
+    this.$nextBtn.addEventListener('click', this.moveRightHandler.bind(this));
   }
 }
