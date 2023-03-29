3c20078602b97a46887430c63f74895b90a7d847
sarang_daddy <sarang_daddy@naver.com>
2023-03-29 17:14:50 +0900

    [Feat] 검색바 클릭시 추천검색어(랜덤) 10개가 노출되는 기능 추가
    - json-server에서 받아온 data에서 랜덤으로 10개의 키워드 색출
    - choiceKeywords() 함수로 색출한 키워드를 element로 생성

--- js/searchBar.js
+++ js/searchBar.js
@@ -1,43 +1,22 @@
-function getData() {
-  fetch("http://localhost:3000/goods")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-}
+const showSearchBar = () => {
+  const homeHeaderSearch = document.querySelector(".homeHeader__search");
+  const headerFlyout = document.querySelector("#header-flyout");
+  const dimmForHeader = document.querySelector(".dimm--header");
 
-getData();
-
-const homeHeaderSearch = document.querySelector(".homeHeader__search");
-const headerFlyout = document.querySelector("#header-flyout");
-const dimmForHeader = document.querySelector(".dimm--header");
-
-const showHeaderFlyout = () => {
-  headerFlyout.classList.remove("hide-opacity");
-  headerFlyout.classList.add("show-opacity");
-  dimmForHeader.classList.remove("hidden");
-};
-
-const hideHeaderFlyput = () => {
-  headerFlyout.classList.add("hide-opacity");
-  headerFlyout.classList.remove("show-opacity");
-  dimmForHeader.classList.add("hidden");
-};
-
-homeHeaderSearch.addEventListener("click", showHeaderFlyout);
-dimmForHeader.addEventListener("click", hideHeaderFlyput);
-
-/*
-function getData() {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://localhost:3000/goods');
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      console.log(JSON.parse(xhr.response));
-    } else {
-      console.error(`Error ${xhr.status} occurred while receiving the data.`);
-    }
+  const showHeaderFlyout = () => {
+    headerFlyout.classList.remove("hide-opacity");
+    headerFlyout.classList.add("show-opacity");
+    dimmForHeader.classList.remove("hidden");
   };
-  xhr.send();
-}
 
-getData();
-*/
+  const hideHeaderFlyput = () => {
+    headerFlyout.classList.add("hide-opacity");
+    headerFlyout.classList.remove("show-opacity");
+    dimmForHeader.classList.add("hidden");
+  };
+
+  homeHeaderSearch.addEventListener("click", showHeaderFlyout);
+  dimmForHeader.addEventListener("click", hideHeaderFlyput);
+};
+
+showSearchBar();
