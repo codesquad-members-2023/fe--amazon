da1d460f423ea1269ec231fb3f5b7802b17ce903
sarang_daddy <sarang_daddy@naver.com>
2023-03-29 15:28:42 +0900

    [Feat] 검색기록창 모달 기능 추가
    - 검색창 클릭시 검색기록창 오픈
    - 딤처리 효과 적용
    - 딤 영역 클릭시 검색기록창 사라짐
    - 헤더 호버영역 선택시 검색기록창 사라짐

--- js/searchBar.js
+++ js/searchBar.js
@@ -6,6 +6,25 @@ function getData() {
 
 getData();
 
+const homeHeaderSearch = document.querySelector(".homeHeader__search");
+const headerFlyout = document.querySelector("#header-flyout");
+const dimmForHeader = document.querySelector(".dimm--header");
+
+const showHeaderFlyout = () => {
+  headerFlyout.classList.remove("hide-opacity");
+  headerFlyout.classList.add("show-opacity");
+  dimmForHeader.classList.remove("hidden");
+};
+
+const hideHeaderFlyput = () => {
+  headerFlyout.classList.add("hide-opacity");
+  headerFlyout.classList.remove("show-opacity");
+  dimmForHeader.classList.add("hidden");
+};
+
+homeHeaderSearch.addEventListener("click", showHeaderFlyout);
+dimmForHeader.addEventListener("click", hideHeaderFlyput);
+
 /*
 function getData() {
   const xhr = new XMLHttpRequest();
