2eb5da46a41b32dad81937d8d34771f71b5a12f1
sjuhan123 <sjuhan123@gmail.com>
2023-03-30 14:38:58 +0900

    refactor. carousel 무한 자동 재생 로직 변경
    setInterval 대신 requestAnimationFrame 사용

--- src/js/carousel/carouselSlideMotion.js
+++ src/js/carousel/carouselSlideMotion.js
@@ -5,8 +5,8 @@ export class CarouselSlideMotion {
     this.btnLayer = btnLayer;
     [this.leftBtn, this.rightBtn] = [...this.btnLayer.childNodes].filter(node => node.nodeName != '#text');
     this.cardLayer = findSiblingForward(this.btnLayer, 'carousel-card__container');
-    this.TRANSITION_DURATION = 2;
-    this.AUTO_SLIDE_DURATION = 5000;
+    this.TRANSITION_DURATION = 1000;
+    this.AUTO_SLIDE_DURATION = 10000;
   }
 
   init(){
@@ -15,18 +15,29 @@ export class CarouselSlideMotion {
   }
 
   startAutoSlide(){
-    setInterval(() => {
+    this.start = null;
+    this.raf = requestAnimationFrame((timestamp) => this.slideRight(timestamp));
+  }
+
+  slideRight(timestamp){
+    if(this.start === null) this.start = timestamp;
+    const time = timestamp - this.start;
+    if(time > 5000){
       this.cardLayer.style.transitionTimingFunction = 'ease-in-out';
-      this.cardLayer.style.transitionDuration = `${this.TRANSITION_DURATION}s`;
+      this.cardLayer.style.transitionDuration = `${this.TRANSITION_DURATION}ms`;
       this.cardLayer.style.transform = `translateX(-100vw)`;
       this.cardLayer.ontransitionend = () => this.whenTransitionEnd();
-    }, this.AUTO_SLIDE_DURATION);
+      cancelAnimationFrame(this.raf);
+      this.start = timestamp;1  
+      requestAnimationFrame(this.slideRight.bind(this));
+    }
+    requestAnimationFrame(this.slideRight.bind(this));
   }
 
   addClickEvt(){
     this.leftBtn.addEventListener('click', () => {
       this.cardLayer.style.transitionTimingFunction = 'ease-in-out';
-      this.cardLayer.style.transitionDuration = `${this.TRANSITION_DURATION}s`;
+      this.cardLayer.style.transitionDuration = `${this.TRANSITION_DURATION}ms`;
       this.cardLayer.style.transform = `translateX(-100vw)`;
       this.cardLayer.ontransitionend = () => this.whenTransitionEnd();
     })
@@ -34,7 +45,7 @@ export class CarouselSlideMotion {
     this.rightBtn.addEventListener('click', () => {
       const DIRECTION = 'Right'
       this.cardLayer.style.transitionTimingFunction = 'ease-in-out';
-      this.cardLayer.style.transitionDuration = `${this.TRANSITION_DURATION}s`;
+      this.cardLayer.style.transitionDuration = `${this.TRANSITION_DURATION}ms`;
       this.cardLayer.style.transform = `translateX(100vw)`;
       this.cardLayer.ontransitionend = () => this.whenTransitionEnd(DIRECTION);
     })
