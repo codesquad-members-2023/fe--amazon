105e51750c69472db177eff5b4eb1df847d18f7e
sarang_daddy <sarang_daddy@naver.com>
2023-03-30 15:06:07 +0900

    [Style] 매직넘버 수정

--- js/bannerApp.js
+++ js/bannerApp.js
@@ -3,9 +3,11 @@ const banners = new Banner();
 banners.setBanners();
 track.addEvent();
 
+const bannerUIMoveTime = 10000;
+
 const bannerTimer = () =>
   setInterval(() => {
     track.moveTrack("left");
-  }, 10000);
+  }, bannerUIMoveTime);
 
 bannerTimer();
