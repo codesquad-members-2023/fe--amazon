d725eee1d1a8a81df36ff73db98a5d80e0b5fbb8
안예림 <an-yelim@an-yelim-ui-MacBookAir.local>
2023-03-31 17:53:45 +0900

    refactor: 슬라이더 뒤에 사진 하나 추가
    무한 슬라이더에 적용하려고 사진을 추가하였습니다.

--- scripts/carousel/carousel.js
+++ scripts/carousel/carousel.js
@@ -3,7 +3,6 @@ class Carousel {
     this.carouselRightBtn = document.querySelector(".carousel_button-right");
     this.carouselLeftBtn = document.querySelector(".carousel_button-left");
     this.carouselContainer = document.querySelector(".carousel_container");
-    this.slideKyeframe = document.styleSheets;
     this.digit = 0;
   }
   init() {
@@ -12,10 +11,12 @@ class Carousel {
   moveRight() {
     this.digit -= 1500;
     this.carouselContainer.style.transform = `translate3d(${this.digit}px, 0, 0)`;
+    this.carouselContainer.style.transitionDuration = "500ms";
   }
   moveLeft() {
     this.digit += 1500;
     this.carouselContainer.style.transform = `translate3d(${this.digit}px, 0, 0)`;
+    this.carouselContainer.style.transitionDuration = "500ms";
   }
   eventlistner() {
     this.carouselRightBtn.addEventListener("click", this.moveRight.bind(this));
