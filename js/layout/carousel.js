da0c7a77a484c27a04fc17f78893a7932058f3f3
saseung <seung0122@naver.com>
2023-03-31 15:55:56 +0900

    ✨ feat: 캐로셀 무한슬라이더 구현

--- js/layout/carousel.js
+++ js/layout/carousel.js
@@ -2,24 +2,31 @@ import { $ } from "../utils.js";
 
 class Carousel {
   constructor() {
-    this.leftButton = $(".left-button");
-    this.rightButton = $(".right-button");
-    this.slideList = $(".slide__list");
-    this.curIndex = 0;
+    this.prevButton = $(".prev-button");
+    this.nextButton = $(".next-button");
+    this.slideList = $(".slide-list");
+    this.content = $(".slide-item");
   }
 
   init() {
-    this.moveSlideList();
+    this.addEvent();
   }
 
-  moveSlideList() {
-    this.leftButton.addEventListener("click", () => {
-      this.slideList.style.transform = "translateX(-100%)";
-    });
+  addEvent() {
+    this.prevButton.addEventListener("click", this.translateContainer.bind(this, 1));
+    this.nextButton.addEventListener("click", this.translateContainer.bind(this, -1));
+  }
 
-    this.rightButton.addEventListener("click", () => {
-      this.slideList.style.transform = "translateX(100%)";
-    });
+  translateContainer(direction) {
+    const selectedButton = direction === 1 ? "prevButton" : "nextButton";
+    this.slideList.style.transitionDuration = "500ms";
+    this.slideList.style.transform = `translateX(${direction * 100}%)`;
+    this.slideList.ontransitionend = () => this.reorganizeEl(selectedButton);
+  }
+
+  reorganizeEl(selectedButton) {
+    this.slideList.removeAttribute("style");
+    selectedButton === "prevButton" ? this.slideList.insertBefore(this.slideList.lastElementChild, this.slideList.firstElementChild) : this.slideList.appendChild(this.slideList.firstElementChild);
   }
 }
 
